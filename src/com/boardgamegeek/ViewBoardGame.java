b9e9b2c44f2dc7c93c534331b7eef0e99c578c83
Chris Comeaux <chris.comeaux@gmail.com>
2009-09-15 02:38:36 +0000

    Fixed bug caused by using the back button to return to a game (the game is re-retrieved). This will be improved once we add a cache.

--- src/com/boardgamegeek/ViewBoardGame.java
+++ src/com/boardgamegeek/ViewBoardGame.java
@@ -67,7 +67,6 @@ public class ViewBoardGame extends TabActivity {
 		if (gameId == null || gameId.length() == 0){
 			gameId = getIntent().getExtras().getString(gameIdKey);
 		}
-		getBoardGame();
 	}
 
 	@Override
@@ -76,6 +75,7 @@ public class ViewBoardGame extends TabActivity {
 
 		// get preferences
 		getPreferences();
+		getBoardGame();
 	}
 
 	@Override
