4c36dc89a63c1377dedcde6d9c600db37071d96c
Chris Comeaux <chris.comeaux@gmail.com>
2010-07-15 04:26:52 +0000

    Merge of collections branch for 2.3 Release

--- src/com/boardgamegeek/model/Expansion.java
+++
@@ -1,9 +0,0 @@
-package com.boardgamegeek.model;
-
-public class Expansion extends Entity {
-
-	public Expansion(int id, String name) {
-		super(id, name);
-	}
-
-}
--- src/com/boardgamegeek/model/Mechanic.java
+++
@@ -1,7 +0,0 @@
-package com.boardgamegeek.model;
-
-public class Mechanic extends Entity {
-	public Mechanic(int id, String name) {
-		super(id, name);
-	}
-}
