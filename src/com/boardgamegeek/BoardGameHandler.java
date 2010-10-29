a91271419a947a475c4a439c8c2884676f16bec3
Chris Comeaux <chris.comeaux@gmail.com>
2010-10-29 04:16:45 +0000

    Port rank parsing fix from trunk

--- src/com/boardgamegeek/BoardGameHandler.java
+++ src/com/boardgamegeek/BoardGameHandler.java
@@ -56,7 +56,13 @@ public class BoardGameHandler extends DefaultHandler {
 				objectId = Utility.parseInt(idAttribute);
 			}
 		} else if (isStats) {
-			if (localName.equals("ranks")) {
+			if (isRanks) {
+				String rankType = atts.getValue("type");
+				if ("subtype".equalsIgnoreCase(rankType)) {
+					String rankValue = atts.getValue("value");
+					boardGame.setRank(Utility.parseInt(rankValue));
+				}
+			} else if (localName.equals("ranks")) {
 				isRanks = true;
 			}
 		} else if (localName.equals("poll")) {
@@ -152,14 +158,8 @@ public class BoardGameHandler extends DefaultHandler {
 			} else if (isRanks && localName.equals("ranks")) {
 				isRanks = false;
 			} else if (isRanks) {
-				if (localName.equalsIgnoreCase("rankobjecttype") && currentElement != null
-					&& currentElement.toString().equalsIgnoreCase("subtype")) {
-					rankType = "boardgame";
-				} else if (localName.equalsIgnoreCase("rankvalue") && rankType.equalsIgnoreCase("boardgame")
-					&& currentElement != null) {
-					boardGame.setRank(Utility.parseInt(currentElement.toString()));
-				} else if (localName.equalsIgnoreCase("rank")) {
-					rankType = "";
+				if (localName.equalsIgnoreCase("ranks")) {
+					isRanks = false;
 				}
 			} else if (localName.equals("stddev")) {
 				boardGame.setStandardDeviation(Utility.parseDouble(currentElement.toString()));
