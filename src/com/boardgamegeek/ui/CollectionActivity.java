681895d86f60b55079d8dc12153664059865b1e3
Chris Comeaux <chris.comeaux@gmail.com>
2011-04-22 03:07:02 +0000

    Download thumbnails in CollectionActivity.

--- src/com/boardgamegeek/ui/CollectionActivity.java
+++ src/com/boardgamegeek/ui/CollectionActivity.java
@@ -1,5 +1,8 @@
 package com.boardgamegeek.ui;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -7,12 +10,15 @@ import android.database.ContentObserver;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.BaseColumns;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -26,7 +32,8 @@ import com.boardgamegeek.util.NotifyingAsyncQueryHandler;
 import com.boardgamegeek.util.NotifyingAsyncQueryHandler.AsyncQueryListener;
 import com.boardgamegeek.util.UIUtils;
 
-public class CollectionActivity extends ListActivity implements AsyncQueryListener {
+public class CollectionActivity extends ListActivity implements AsyncQueryListener, AbsListView.OnScrollListener {
+	private static final String TAG = "CollectionActivity";
 
 	private static final long OBSERVER_THROTTLE_IN_MILLIS = 10000; // 10 seconds
 
@@ -35,6 +42,8 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 	private Uri mUri;
 	private CollectionObserver mGameObserver;
 	private long mLastUpdated;
+	private final BlockingQueue<String> mThumbnailQueue = new ArrayBlockingQueue<String>(12);
+	private ThumbnailTask mThumbnailTask;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,8 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 
 		UIUtils.setTitle(this);
 		UIUtils.allowTypeToSearch(this);
+
+		getListView().setOnScrollListener(this);
 
 		mGameObserver = new CollectionObserver(null);
 		mAdapter = new CollectionAdapter(this);
@@ -57,6 +68,20 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 	protected void onStart() {
 		super.onStart();
 		getContentResolver().registerContentObserver(mUri, true, mGameObserver);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mThumbnailTask = new ThumbnailTask();
+		mThumbnailTask.execute();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mThumbnailQueue.clear();
+		mThumbnailTask.cancel(true);
 	}
 
 	@Override
@@ -134,12 +159,14 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 				yearPublished = "" + year;
 			}
 			holder.year.setText(yearPublished);
+			holder.thumbnailUrl = cursor.getString(Query.THUMBNAIL_URL);
 
-			Drawable thumbnail = ImageCache.getDrawableFromCache(cursor.getString(Query.THUMBNAIL_URL));
+			Drawable thumbnail = ImageCache.getDrawableFromCache(holder.thumbnailUrl);
 
 			if (thumbnail == null) {
 				holder.thumbnail.setVisibility(View.GONE);
 			} else {
+				holder.thumbnailUrl = null;
 				holder.thumbnail.setVisibility(View.VISIBLE);
 				holder.thumbnail.setImageDrawable(thumbnail);
 			}
@@ -150,11 +177,41 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 		TextView name;
 		TextView year;
 		ImageView thumbnail;
+		String thumbnailUrl;
 
 		public ViewHolder(View view) {
 			name = (TextView) view.findViewById(R.id.name);
 			year = (TextView) view.findViewById(R.id.year);
 			thumbnail = (ImageView) view.findViewById(R.id.listThumbnail);
+		}
+	}
+
+	private class ThumbnailTask extends AsyncTask<Void, String, Void> {
+
+		private ListView mView;
+
+		@Override
+		protected void onPreExecute() {
+			mView = CollectionActivity.this.getListView();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			while (!isCancelled()) {
+				try {
+					String url = mThumbnailQueue.take();
+					ImageCache.getImage(CollectionActivity.this, url);
+					publishProgress(url);
+				} catch (InterruptedException e) {
+					Log.w(TAG, e.toString());
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(String... values) {
+			mView.invalidateViews();
 		}
 	}
 
@@ -169,5 +226,23 @@ public class CollectionActivity extends ListActivity implements AsyncQueryListen
 		// int GAME_NAME = 4;
 		int GAME_ID = 5;
 		int THUMBNAIL_URL = 6;
+	}
+
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		// do nothing
+	}
+
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		if (scrollState == SCROLL_STATE_IDLE) {
+			final int count = view.getChildCount();
+			for (int i = 0; i < count; i++) {
+				ViewHolder vh = (ViewHolder) view.getChildAt(i).getTag();
+				if (vh.thumbnail.getVisibility() == View.GONE && vh.thumbnailUrl != null) {
+					mThumbnailQueue.offer(vh.thumbnailUrl);
+				}
+			}
+		} else {
+			mThumbnailQueue.clear();
+		}
 	}
 }
