810d730eebf4f4237e6f7b3561762325a5d6646f
Chris Comeaux <chris.comeaux@gmail.com>
2011-06-17 03:25:50 +0000

    Use better display names in the CollectionStatusFilter

--- BoardGameGeek/src/com/boardgamegeek/ui/dialog/CollectionStatusFilter.java
+++ BoardGameGeek/src/com/boardgamegeek/ui/dialog/CollectionStatusFilter.java
@@ -8,95 +8,74 @@ import android.content.DialogInterface;
 
 import com.boardgamegeek.R;
 import com.boardgamegeek.data.CollectionFilter;
-import com.boardgamegeek.provider.BggContract.Collection;
 import com.boardgamegeek.ui.CollectionActivity;
 
 public class CollectionStatusFilter {
-	
-	final String[] items = {Collection.STATUS_OWN, Collection.STATUS_FOR_TRADE,
-			Collection.STATUS_PREORDERED, Collection.STATUS_PREVIOUSLY_OWNED,
-			Collection.STATUS_WANT, Collection.STATUS_WISHLIST};
-	boolean[] mSelected = new boolean[items.length];
-	
-	public void createDialog(final CollectionActivity activity) {		
+
+	private String[] statusEntries;
+	private String[] items;
+	private boolean[] mSelected;
+
+	public void createDialog(final CollectionActivity activity) {
+		if (mSelected == null) {
+			statusEntries = activity.getResources().getStringArray(R.array.collection_status_filter_entries);
+			items = activity.getResources().getStringArray(R.array.collection_status_filter_values);
+			mSelected = new boolean[items.length];
+		}
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 		builder.setTitle(R.string.menu_collection_status);
-		builder.setMultiChoiceItems(items, mSelected, new DialogInterface.OnMultiChoiceClickListener() {
-			
+		builder.setMultiChoiceItems(statusEntries, mSelected, new DialogInterface.OnMultiChoiceClickListener() {
 			@Override
-			public void onClick(DialogInterface dialog, int which,
-					boolean isChecked) {
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 				mSelected[which] = isChecked;
 			}
 		});
 		builder.setNeutralButton("Or", new DialogInterface.OnClickListener() {
-			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				CollectionFilter filter = new CollectionFilter();
-				String name = "", selection = "";
-				List<String> selectionArgs = new ArrayList<String>(mSelected.length);
-								
-				for(int i = 0; i < mSelected.length; i++) {					
-					if(mSelected[i]) {
-						name += items[i] + " | ";
-						selection += items[i] + ">=? OR ";
-						selectionArgs.add("1");
-					}																			
-				}
-				
-				//Remove the last trailing combiners
-				name = name.substring(0, name.length()-2);
-				selection = selection.substring(0, selection.length()-4);
-				
-		    	filter.
-				name(name).
-				selection(selection).
-				selectionargs(selectionArgs.toArray(new String[selectionArgs.size()])).
-				id(R.id.menu_collection_status);
-				
+				CollectionFilter filter = createFilter("OR", "|");
 				activity.addFilter(filter);
 			}
-		}).
-		setPositiveButton("And", new DialogInterface.OnClickListener() {
-			
+		}).setPositiveButton("And", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				CollectionFilter filter = new CollectionFilter();
-				String name = "", selection = "";
-				List<String> selectionArgs = new ArrayList<String>(mSelected.length);
-								
-				for(int i = 0; i < mSelected.length; i++) {					
-					if(mSelected[i]) {
-						name += items[i] + " & ";
-						selection += items[i] + ">=? AND ";
-						selectionArgs.add("1");
-					}																			
-				}
-				
-				//Remove the last trailing combiners
-				name = name.substring(0, name.length()-2);
-				selection = selection.substring(0, selection.length()-5);
-				
-		    	filter.
-				name(name).
-				selection(selection).
-				selectionargs(selectionArgs.toArray(new String[selectionArgs.size()])).
-				id(R.id.menu_collection_status);
-				
+				CollectionFilter filter = createFilter("AND", "&");
 				activity.addFilter(filter);
 			}
-		})
-		.setNegativeButton("Clear", new DialogInterface.OnClickListener() {
-    		
+		}).setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				CollectionFilter filter = new CollectionFilter().id(R.id.menu_collection_status);
 				activity.removeFilter(filter);
-			}        		
-    	});
-		
+			}
+		});
+
 		AlertDialog alert = builder.create();
 		alert.show();
+	}
+
+	private CollectionFilter createFilter(String selectionConjunction, String nameDelimiter) {
+		CollectionFilter filter = new CollectionFilter();
+		String name = "";
+		String selection = "";
+		List<String> selectionArgs = new ArrayList<String>(mSelected.length);
+
+		for (int i = 0; i < mSelected.length; i++) {
+			if (mSelected[i]) {
+				name += statusEntries[i] + " " + nameDelimiter + " ";
+				selection += items[i] + ">=? " + selectionConjunction + " ";
+				selectionArgs.add("1");
+			}
+		}
+
+		// Remove the last trailing combiners
+		name = name.substring(0, name.length() - (2 + nameDelimiter.length()));
+		selection = selection.substring(0, selection.length() - (2 + selectionConjunction.length()));
+
+		filter.name(name).selection(selection).selectionargs(selectionArgs.toArray(new String[selectionArgs.size()]))
+				.id(R.id.menu_collection_status);
+		return filter;
 	}
 }
