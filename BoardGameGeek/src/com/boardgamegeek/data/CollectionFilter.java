dd159956aa9d1273a542bd950265ade36a05e9b8
Chris Comeaux <chris.comeaux@gmail.com>
2011-06-18 03:08:47 +0000

    Save filter state (like during orientation change)

--- BoardGameGeek/src/com/boardgamegeek/data/CollectionFilter.java
+++ BoardGameGeek/src/com/boardgamegeek/data/CollectionFilter.java
@@ -1,11 +1,17 @@
 package com.boardgamegeek.data;
 
-public class CollectionFilter {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CollectionFilter implements Parcelable {
 
 	private String displayText;
 	private String selection;
 	private String[] selectionArgs = {};
 	private int id;
+
+	public CollectionFilter() {
+	}
 
 	public String getSelection() {
 		return selection;
@@ -57,5 +63,35 @@ public class CollectionFilter {
 	@Override
 	public int hashCode() {
 		return this.getId();
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int flags) {
+		out.writeString(displayText);
+		out.writeString(selection);
+		out.writeStringArray(selectionArgs);
+		out.writeInt(id);
+	}
+
+	public static final Parcelable.Creator<CollectionFilter> CREATOR = new Parcelable.Creator<CollectionFilter>() {
+		public CollectionFilter createFromParcel(Parcel in) {
+			return new CollectionFilter(in);
+		}
+
+		public CollectionFilter[] newArray(int size) {
+			return new CollectionFilter[size];
+		}
+	};
+
+	private CollectionFilter(Parcel in) {
+		displayText = in.readString();
+		selection = in.readString();
+		in.readStringArray(selectionArgs);
+		id = in.readInt();
 	}
 }
