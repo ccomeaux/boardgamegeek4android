9de58f6bc3a3daf6335a44026c7188068b746583
Chris Comeaux <chris.comeaux@gmail.com>
2011-06-28 04:11:03 +0000

    Add long click options to SearchResultsActivity and CollectionActivity

---
+++ BoardGameGeek/src/com/boardgamegeek/util/ActivityUtils.java
@@ -0,0 +1,56 @@
+package com.boardgamegeek.util;
+
+import java.net.URLEncoder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+
+import com.boardgamegeek.R;
+import com.boardgamegeek.ui.LogPlayActivity;
+
+public class ActivityUtils {
+
+	public static void shareGame(Context context, int gameId, String gameName) {
+		Resources r = context.getResources();
+		Intent shareIntent = new Intent(Intent.ACTION_SEND);
+		shareIntent.setType("text/plain");
+		shareIntent.putExtra(Intent.EXTRA_SUBJECT, String.format(r.getString(R.string.share_subject), gameName));
+		shareIntent.putExtra(
+				Intent.EXTRA_TEXT,
+				String.format(r.getString(R.string.share_text), gameName, "http://www.boardgamegeek.com/boardgame/"
+						+ gameId));
+		context.startActivity(Intent.createChooser(shareIntent, r.getString(R.string.share_title)));
+	}
+
+	public static void logPlay(Context context, boolean quick, int id, String name, String thumbnailUrl) {
+		Intent intent = new Intent(context, LogPlayActivity.class);
+		intent.setAction(quick ? Intent.ACTION_VIEW : Intent.ACTION_EDIT);
+		intent.putExtra(LogPlayActivity.KEY_GAME_ID, id);
+		intent.putExtra(LogPlayActivity.KEY_GAME_NAME, name);
+		intent.putExtra(LogPlayActivity.KEY_THUMBNAIL_URL, thumbnailUrl);
+		context.startActivity(intent);
+	}
+
+	public static void linkBgg(Context context, int gameId) {
+		link(context, "http://www.boardgamegeek.com/boardgame/" + gameId);
+	}
+
+	public static void linkBgPrices(Context context, String gameName) {
+		link(context, "http://boardgameprices.com/iphone/?s=" + URLEncoder.encode(gameName));
+	}
+
+	public static void linkAmazon(Context context, String gameName) {
+		link(context, "http://www.amazon.com/gp/aw/s.html/?m=aps&k=" + URLEncoder.encode(gameName)
+				+ "&i=toys-and-games&submitSearch=GO");
+	}
+
+	public static void linkEbay(Context context, String gameName) {
+		link(context, "http://shop.mobileweb.ebay.com/searchresults?kw=" + URLEncoder.encode(gameName));
+	}
+
+	private static void link(Context context, String link) {
+		context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
+	}
+}
--- BoardGameGeek/src/com/boardgamegeek/util/UIUtils.java
+++ BoardGameGeek/src/com/boardgamegeek/util/UIUtils.java
@@ -10,6 +10,10 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.text.TextUtils;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -23,6 +27,16 @@ public class UIUtils {
 
 	private Activity mActivity;
 	private static Random mRandom;
+
+	public static final int MENU_ITEM_VIEW = Menu.FIRST;
+	public static final int MENU_ITEM_LOG_PLAY = Menu.FIRST + 1;
+	public static final int MENU_ITEM_QUICK_LOG_PLAY = Menu.FIRST + 2;
+	public static final int MENU_ITEM_SHARE = Menu.FIRST + 3;
+	public static final int MENU_ITEM_LINKS = Menu.FIRST + 4;
+	public static final int MENU_ITEM_LINK_BGG = Menu.FIRST + 5;
+	public static final int MENU_ITEM_LINK_BG_PRICES = Menu.FIRST + 6;
+	public static final int MENU_ITEM_LINK_AMAZON = Menu.FIRST + 7;
+	public static final int MENU_ITEM_LINK_EBAY = Menu.FIRST + 8;
 
 	public UIUtils(Activity activity) {
 		mActivity = activity;
@@ -188,5 +202,20 @@ public class UIUtils {
 				}
 			});
 		return builder.create();
+	}
+
+	public static void createBoardgameContextMenu(ContextMenu menu, ContextMenuInfo menuInfo, String gameName) {
+		// Setup the menu header
+		menu.setHeaderTitle(gameName);
+
+		menu.add(0, MENU_ITEM_VIEW, 0, R.string.menu_display_game);
+		menu.add(0, MENU_ITEM_LOG_PLAY, 0, R.string.menu_log_play);
+		menu.add(0, MENU_ITEM_QUICK_LOG_PLAY, 0, R.string.menu_log_play_quick);
+		menu.add(0, MENU_ITEM_SHARE, 0, R.string.menu_share);
+		SubMenu links = menu.addSubMenu(0, MENU_ITEM_LINKS, 0, R.string.menu_links);
+		links.add(0, MENU_ITEM_LINK_BGG, 0, R.string.menu_link_bgg);
+		links.add(0, MENU_ITEM_LINK_BG_PRICES, 0, R.string.menu_link_bg_prices);
+		links.add(0, MENU_ITEM_LINK_AMAZON, 0, R.string.menu_link_amazon);
+		links.add(0, MENU_ITEM_LINK_EBAY, 0, R.string.menu_link_ebay);
 	}
 }
