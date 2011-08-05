1dc6336f3d643c00922f4d3f2b68d2cc224a0fd7
Chris Comeaux <chris.comeaux@gmail.com>
2011-08-05 18:05:53 +0000

    In CommentsActivity, retain state on orientation change

--- BoardGameGeek/src/com/boardgamegeek/io/Comment.java
+++ BoardGameGeek/src/com/boardgamegeek/io/Comment.java
@@ -1,7 +1,41 @@
 package com.boardgamegeek.io;
 
-public class Comment {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Comment implements Parcelable {
 	public String Username;
 	public String Rating;
 	public String Value;
+
+	public Comment() {
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(Username);
+		dest.writeString(Rating);
+		dest.writeString(Value);
+	}
+
+	public static final Parcelable.Creator<Comment> CREATOR = new Parcelable.Creator<Comment>() {
+		public Comment createFromParcel(Parcel in) {
+			return new Comment(in);
+		}
+
+		public Comment[] newArray(int size) {
+			return new Comment[size];
+		}
+	};
+
+	private Comment(Parcel in) {
+		Username = in.readString();
+		Rating = in.readString();
+		Value = in.readString();
+	}
 }
