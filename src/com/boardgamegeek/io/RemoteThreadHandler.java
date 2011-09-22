0ccebc97dd3ad943b1c42001093ee38dd4e7762e
Chris Comeaux <chris.comeaux@gmail.com>
2011-09-22 01:40:37 +0000

    Change display of forum date and times

--- src/com/boardgamegeek/io/RemoteThreadHandler.java
+++ src/com/boardgamegeek/io/RemoteThreadHandler.java
@@ -12,15 +12,17 @@ import java.util.List;
 import org.xmlpull.v1.XmlPullParserException;
 
 import com.boardgamegeek.model.ThreadArticle;
+import com.boardgamegeek.util.DateTimeUtils;
+import com.boardgamegeek.util.StringUtils;
 
 public class RemoteThreadHandler extends RemoteBggHandler {
 
 	private List<ThreadArticle> mArticles = new ArrayList<ThreadArticle>();
-	
+
 	public List<ThreadArticle> getResults() {
 		return mArticles;
 	}
-	
+
 	@Override
 	protected void clearResults() {
 		mArticles.clear();
@@ -38,53 +40,59 @@ public class RemoteThreadHandler extends RemoteBggHandler {
 
 	@Override
 	protected void parseItems() throws XmlPullParserException, IOException {
-		
+
 		final int depth = mParser.getDepth();
 		int type;
 		while (((type = mParser.next()) != END_TAG || mParser.getDepth() > depth) && type != END_DOCUMENT) {
 			if (type == START_TAG && Tags.ARTICLE.equals(mParser.getName())) {
 				final String userName = mParser.getAttributeValue(null, Tags.USERNAME);
-				
+				final long postDate = DateTimeUtils.parseDate(mParser.getAttributeValue(null, Tags.POST_DATE));
+				final long editDate = DateTimeUtils.parseDate(mParser.getAttributeValue(null, Tags.EDIT_DATE));
+				final int numEdits = StringUtils.parseInt(mParser.getAttributeValue(null, Tags.NUM_EDITS));
+
 				final ThreadArticle article = parseItem();
 				article.username = userName;
+				article.postdate = postDate;
+				article.editdate = editDate;
+				article.numedits = numEdits;
 				mArticles.add(article);
 			}
 		}
 	}
-	
+
 	private ThreadArticle parseItem() throws XmlPullParserException, IOException {
 		String tag = null;
 		final ThreadArticle article = new ThreadArticle();
-		
+
 		final int depth = mParser.getDepth();
 		int type;
 		while (((type = mParser.next()) != END_TAG || mParser.getDepth() > depth) && type != END_DOCUMENT) {
-			
+
 			if (type == START_TAG) {
 				tag = mParser.getName();
 			}
 			if (type == TEXT) {
 				final String text = mParser.getText();
-/*				if (Tags.SUBJECT.equals(tag)) {
-					thread.subject = text;
-				}
-				else*/ if (Tags.BODY.equals(tag)) {
+				if (Tags.SUBJECT.equals(tag)) {
+					article.subject = text;
+				} else if (Tags.BODY.equals(tag)) {
 					article.body = text;
 				}
-			}
-			else if (type == END_TAG) {
+			} else if (type == END_TAG) {
 				tag = null;
 			}
 		}
-		
+
 		return article;
 	}
 
 	private interface Tags {
-//		String ARTICLES = "articles";
 		String ARTICLE = "article";
 		String USERNAME = "username";
-//		String SUBJECT = "subject";
+		String POST_DATE = "postdate";
+		String EDIT_DATE = "editdate";
+		String NUM_EDITS = "numedits";
+		String SUBJECT = "subject";
 		String BODY = "body";
 	}
 }
--- src/com/boardgamegeek/util/DateTimeUtils.java
+++ src/com/boardgamegeek/util/DateTimeUtils.java
@@ -1,9 +1,20 @@
 package com.boardgamegeek.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.text.format.DateUtils;
 
 public class DateTimeUtils {
 	
+	private static final String BGG_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+	private static final DateFormat FORMATER = new SimpleDateFormat(BGG_DATE_FORMAT, Locale.US);
+
+	private DateTimeUtils(){}
+
 	public static int howManyDaysOld(long time) {
 		return (int) ((System.currentTimeMillis() - time) / DateUtils.DAY_IN_MILLIS);
 	}
@@ -12,5 +23,31 @@ public class DateTimeUtils {
 		return (int) ((System.currentTimeMillis() - time) / DateUtils.HOUR_IN_MILLIS);
 	}
 
-	private DateTimeUtils(){}
+	/**
+	 * @param inDate date from BGG XMLs
+	 * @return string with parsed date (or original date if parsing not successful)
+	 */
+	public static long parseDate(String inDate) {
+		inDate = removeTimeZone(inDate);
+
+		String outDate;
+		try {
+			final Date date = FORMATER.parse(inDate);
+			outDate = date.toLocaleString();
+		} catch (ParseException e) {
+			outDate = inDate;
+		}
+		
+		long result = Date.parse(outDate);
+		
+		return result;
+	}
+
+	private static String removeTimeZone(String inDate) {
+		int index = inDate.lastIndexOf("-");
+		if (index > 0) {
+			inDate = inDate.substring(0, index);
+		}
+		return inDate;
+	}
 }
