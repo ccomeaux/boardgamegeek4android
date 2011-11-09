a61f183e07d5bcdc01757617a02280dd186be8a9
Chris Comeaux <chris.comeaux@gmail.com>
2011-11-09 15:08:36 +0000

    Improve syncing to include the last 50 games, if everything else is newer than a month.

--- src/com/boardgamegeek/service/SyncCollectionDetail.java
+++ src/com/boardgamegeek/service/SyncCollectionDetail.java
@@ -18,8 +18,10 @@ import com.boardgamegeek.util.HttpUtils;
 
 public class SyncCollectionDetail extends SyncTask {
 
-	private static final int SYNC_GAME_DETAIL_DAYS = 7;
 	private static final int GAMES_PER_FETCH = 25;
+	// TODO Perhaps move these contsants into preferences
+	private static final int SYNC_GAME_AGE_IN_DAYS = 30;
+	private static final int SYNC_GAME_LIMIT = 50;
 
 	private RemoteExecutor mRemoteExecutor;
 
@@ -31,27 +33,40 @@ public class SyncCollectionDetail extends SyncTask {
 		ContentResolver resolver = context.getContentResolver();
 
 		try {
-			List<String> ids = new ArrayList<String>();
-			long days = System.currentTimeMillis() - (SYNC_GAME_DETAIL_DAYS * DateUtils.DAY_IN_MILLIS);
+			// Update games that haven't been synced or haven't been synced in a while
+			long days = System.currentTimeMillis() - (SYNC_GAME_AGE_IN_DAYS * DateUtils.DAY_IN_MILLIS);
 			cursor = resolver.query(Games.CONTENT_URI, new String[] { Games.GAME_ID }, SyncColumns.UPDATED + "<? OR "
 					+ SyncColumns.UPDATED + " IS NULL", new String[] { String.valueOf(days) }, null);
-			while (cursor.moveToNext()) {
-				final String id = cursor.getString(0);
-				ids.add(id);
-				if (ids.size() >= GAMES_PER_FETCH) {
-					mRemoteExecutor.executeGet(HttpUtils.constructGameUrl(ids), new RemoteGameHandler());
-					ids.clear();
-				}
-			}
-
-			if (ids.size() > 0) {
-				mRemoteExecutor.executeGet(HttpUtils.constructGameUrl(ids), new RemoteGameHandler());
+			if (cursor.moveToFirst()) {
+				fetchGames(cursor);
+			} else {
+				// If everything is relatively up to date, sync the oldest games
+				cursor = resolver.query(Games.CONTENT_URI, new String[] { Games.GAME_ID }, null, null, Games.UPDATED
+						+ " LIMIT " + SYNC_GAME_LIMIT);
+				fetchGames(cursor);
 			}
 
 		} finally {
 			if (cursor != null) {
 				cursor.close();
 			}
+		}
+	}
+
+	private void fetchGames(Cursor cursor) throws HandlerException {
+		List<String> ids = new ArrayList<String>();
+		cursor.moveToPosition(-1);
+		while (cursor.moveToNext()) {
+			final String id = cursor.getString(0);
+			ids.add(id);
+			if (ids.size() >= GAMES_PER_FETCH) {
+				mRemoteExecutor.executeGet(HttpUtils.constructGameUrl(ids), new RemoteGameHandler());
+				ids.clear();
+			}
+		}
+
+		if (ids.size() > 0) {
+			mRemoteExecutor.executeGet(HttpUtils.constructGameUrl(ids), new RemoteGameHandler());
 		}
 	}
 
