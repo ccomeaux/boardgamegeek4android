561fae66351a284fa5a2986db50a0271f102a674
Chris Comeaux <chris.comeaux@gmail.com>
2011-12-20 02:51:45 +0000

    Delete plays not found in the sync

--- BoardGameGeek/src/com/boardgamegeek/service/SyncPlays.java
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncPlays.java
@@ -1,6 +1,8 @@
 package com.boardgamegeek.service;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.util.Log;
 
 import com.boardgamegeek.BggApplication;
 import com.boardgamegeek.R;
@@ -8,27 +10,40 @@ import com.boardgamegeek.io.RemoteExecutor;
 import com.boardgamegeek.io.RemotePlaysHandler;
 import com.boardgamegeek.io.XmlHandler;
 import com.boardgamegeek.io.XmlHandler.HandlerException;
+import com.boardgamegeek.provider.BggContract.Plays;
 import com.boardgamegeek.util.HttpUtils;
 
 public class SyncPlays extends SyncTask {
+	private final static String TAG = "SyncPlays";
 
 	@Override
 	public void execute(RemoteExecutor executor, Context context) throws HandlerException {
 
+		ContentResolver resolver = context.getContentResolver();
 		XmlHandler handler = new RemotePlaysHandler();
 		String username = BggApplication.getInstance().getUserName();
+		String minDate = BggApplication.getInstance().getMinPlayDate();
+		String maxDate = BggApplication.getInstance().getMaxPlayDate();
+		long startTime = System.currentTimeMillis();
 
 		String url = HttpUtils.constructPlaysUrlNew(username);
 		int page = 1;
 		while (executor.executeGet(url + "&page=" + page, handler)) {
 			page++;
 		}
+		String[] selectionArgs = new String[] { String.valueOf(startTime), minDate };
+		int count = resolver.delete(Plays.CONTENT_URI, Plays.UPDATED_LIST + "<? AND " + Plays.DATE + ">=?",
+				selectionArgs);
+		Log.i(TAG, "Deleted " + count + " plays");
 
 		url = HttpUtils.constructPlaysUrlOld(username);
 		page = 1;
 		while (executor.executeGet(url + "&page=" + page, handler)) {
 			page++;
 		}
+		selectionArgs = new String[] { String.valueOf(startTime), maxDate };
+		count = resolver.delete(Plays.CONTENT_URI, Plays.UPDATED_LIST + "<? AND " + Plays.DATE + "<=?", selectionArgs);
+		Log.i(TAG, "Deleted " + count + " plays");
 
 		BggApplication.getInstance().putMaxPlayDate("0000-00-00");
 	}
