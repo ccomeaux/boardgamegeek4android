f4deba0bb730fa75a1a64499df07473cef87d5f1
Chris Comeaux <chris.comeaux@gmail.com>
2012-05-11 21:13:50 +0000

    User can now update existing collection filters

--- src/com/boardgamegeek/ui/dialog/SaveFilters.java
+++ src/com/boardgamegeek/ui/dialog/SaveFilters.java
@@ -8,7 +8,9 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,13 +28,15 @@ import com.boardgamegeek.ui.CollectionActivity;
 
 public class SaveFilters {
 
-	public static void createDialog(final CollectionActivity activity, final List<CollectionFilterData> filters) {
+	public static void createDialog(final CollectionActivity activity, String name,
+			final List<CollectionFilterData> filters) {
 
 		LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View layout = inflater.inflate(R.layout.dialog_save_filters,
 				(ViewGroup) activity.findViewById(R.id.layout_root));
 
 		final EditText nameView = (EditText) layout.findViewById(R.id.name);
+		nameView.setText(name);
 		setDescription(filters, layout);
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(activity).setTitle(R.string.save_filters).setView(layout)
@@ -40,21 +44,66 @@ public class SaveFilters {
 
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						String name = nameView.getText().toString().trim();
-						ContentResolver resolver = activity.getContentResolver();
-						// TODO: check name for uniqueness?
-						insert(name, filters, resolver);
-						Toast.makeText(activity, R.string.msg_saved, Toast.LENGTH_SHORT).show();
-						activity.setFilterName(name);
+						final String name = nameView.getText().toString().trim();
+						final ContentResolver resolver = activity.getContentResolver();
+
+						final int filterId = findFilterId(resolver, name);
+						if (filterId > 0) {
+							new AlertDialog.Builder(activity).setTitle(R.string.title_collection_filter_name_in_use)
+									.setMessage(R.string.msg_collection_filter_name_in_use)
+									.setPositiveButton(R.string.update, new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(DialogInterface dialog, int which) {
+											update(resolver, filterId, filters);
+											updateDisplay(activity, name);
+										}
+									}).setNegativeButton(R.string.create, new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(DialogInterface dialog, int which) {
+											insert(resolver, name, filters);
+											updateDisplay(activity, name);
+										}
+									}).create().show();
+
+						} else {
+							insert(resolver, name, filters);
+							updateDisplay(activity, name);
+						}
 					}
 
-					private void insert(String name, final List<CollectionFilterData> filters, ContentResolver resolver) {
+					private int findFilterId(ContentResolver resolver, String name) {
+						Cursor c = resolver.query(CollectionFilters.CONTENT_URI, new String[] { BaseColumns._ID },
+								CollectionFilters.NAME + "=?", new String[] { name }, null);
+						if (c != null) {
+							try {
+								if (c.moveToFirst()) {
+									return c.getInt(0);
+								}
+							} finally {
+								c.close();
+							}
+						}
+						return -1;
+					}
+
+					private void insert(ContentResolver resolver, String name, final List<CollectionFilterData> filters) {
 						ContentValues values = new ContentValues();
 						values.put(CollectionFilters.NAME, name);
 						values.put(CollectionFilters.STARRED, false);
 						Uri filterUri = resolver.insert(CollectionFilters.CONTENT_URI, values);
 						int filterId = CollectionFilters.getFilterId(filterUri);
+						Uri uri = CollectionFilters.buildFilterDetailUri(filterId);
+						insertDetails(resolver, uri, filters);
+					}
 
+					private void update(ContentResolver resolver, int filterId, final List<CollectionFilterData> filters) {
+						Uri uri = CollectionFilters.buildFilterDetailUri(filterId);
+						resolver.delete(uri, null, null);
+						insertDetails(resolver, uri, filters);
+					}
+
+					private void insertDetails(ContentResolver resolver, Uri filterUri,
+							final List<CollectionFilterData> filters) {
 						List<ContentValues> cvs = new ArrayList<ContentValues>(filters.size());
 						for (CollectionFilterData filter : filters) {
 							ContentValues cv = new ContentValues();
@@ -62,8 +111,12 @@ public class SaveFilters {
 							cv.put(CollectionFilterDetails.DATA, filter.flatten());
 							cvs.add(cv);
 						}
-						resolver.bulkInsert(CollectionFilters.buildFilterDetailUri(filterId),
-								cvs.toArray(new ContentValues[cvs.size()]));
+						resolver.bulkInsert(filterUri, cvs.toArray(new ContentValues[cvs.size()]));
+					}
+
+					private void updateDisplay(final CollectionActivity activity, String name) {
+						Toast.makeText(activity, R.string.msg_saved, Toast.LENGTH_SHORT).show();
+						activity.setFilterName(name);
 					}
 				}).setNegativeButton(R.string.cancel, null).setCancelable(true);
 
