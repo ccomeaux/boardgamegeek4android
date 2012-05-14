a41b6d0d52dfb3e8f7e07380bc51493e576ea3f2
Chris Comeaux <chris.comeaux@gmail.com>
2012-05-14 02:03:05 +0000

    More handler refactoring

--- src/com/boardgamegeek/io/RemoteArtistHandler.java
+++ src/com/boardgamegeek/io/RemoteArtistHandler.java
@@ -1,100 +1,61 @@
 package com.boardgamegeek.io;
 
-import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
-import static org.xmlpull.v1.XmlPullParser.END_TAG;
-import static org.xmlpull.v1.XmlPullParser.START_TAG;
-import static org.xmlpull.v1.XmlPullParser.TEXT;
-
-import java.io.IOException;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
-import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.Artists;
 
-public class RemoteArtistHandler extends XmlHandler {
-	private static final String TAG = "RemoteArtistHandler";
-
-	private int mArtistId;
-
-	public RemoteArtistHandler() {
-		super(BggContract.CONTENT_AUTHORITY);
-	}
-
-	public RemoteArtistHandler(int artistId) {
-		super(BggContract.CONTENT_AUTHORITY);
-		mArtistId = artistId;
+public class RemoteArtistHandler extends RemoteProducerHandler {
+	public RemoteArtistHandler(int producerId) {
+		super(producerId);
 	}
 
 	@Override
-	public boolean parse(XmlPullParser parser, ContentResolver resolver, String authority)
-			throws XmlPullParserException, IOException {
-
-		if (mArtistId == 0) {
-			return false;
-		}
-
-		String[] projection = { Artists.ARTIST_ID };
-
-		int type;
-		while ((type = parser.next()) != END_DOCUMENT) {
-			if (type == START_TAG && Tags.PERSON.equals(parser.getName())) {
-
-				Uri uri = Artists.buildArtistUri(mArtistId);
-				Cursor cursor = resolver.query(uri, projection, null, null, null);
-
-				if (!cursor.moveToFirst()) {
-					Log.w(TAG, "Tried to parse artist, but ID not in database: " + mArtistId);
-				} else {
-					parseArtist(parser, resolver, uri);
-				}
-
-				cursor.close();
-			}
-		}
-
-		return false;
+	protected String type() {
+		return "artist";
 	}
 
-	private void parseArtist(XmlPullParser parser, ContentResolver resolver, Uri uri) throws XmlPullParserException,
-			IOException {
-
-		final int depth = parser.getDepth();
-		ContentValues values = new ContentValues();
-		String tag = null;
-
-		int type;
-		while (((type = parser.next()) != END_TAG || parser.getDepth() > depth) && type != END_DOCUMENT) {
-			if (type == START_TAG) {
-				tag = parser.getName();
-			} else if (type == END_TAG) {
-				tag = null;
-			} else if (type == TEXT) {
-				String text = parser.getText();
-
-				if (Tags.NAME.equals(tag)) {
-					values.put(Artists.ARTIST_NAME, text);
-				} else if (Tags.DESCRIPTION.equals(tag)) {
-					values.put(Artists.ARTIST_DESCRIPTION, text);
-				}
-			}
-		}
-
-		values.put(Artists.UPDATED, System.currentTimeMillis());
-		resolver.update(uri, values, null, null);
+	@Override
+	protected Uri createUri() {
+		return Artists.buildArtistUri(mProducerId);
 	}
 
-	private interface Tags {
-		// String PEOPLE = "people";
-		String PERSON = "person";
-		String NAME = "name";
-		String DESCRIPTION = "description";
+	@Override
+	protected String ItemTag() {
+		return "person";
+	}
+
+	@Override
+	protected String nameTag() {
+		return "name";
+	}
+
+	@Override
+	protected String descriptionTag() {
+		return "description";
+	}
+
+	@Override
+	protected String idColumn() {
+		return Artists.ARTIST_ID;
+	}
+
+	@Override
+	protected String nameColumn() {
+		return Artists.ARTIST_NAME;
+	}
+
+	@Override
+	protected String descriptionColumn() {
+		return Artists.ARTIST_DESCRIPTION;
+	}
+
+	@Override
+	protected String updatedColumn() {
+		return Artists.UPDATED;
+	}
+
+	@Override
+	protected String getRootNodeName() {
+		return "people";
 	}
 }
---
+++ src/com/boardgamegeek/io/RemoteProducerHandler.java
@@ -0,0 +1,100 @@
+package com.boardgamegeek.io;
+
+import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
+import static org.xmlpull.v1.XmlPullParser.END_TAG;
+import static org.xmlpull.v1.XmlPullParser.START_TAG;
+import static org.xmlpull.v1.XmlPullParser.TEXT;
+
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+public abstract class RemoteProducerHandler extends RemoteBggHandler {
+	private static final String TAG = "RemotePublisherHandler";
+
+	protected int mProducerId;
+	private int mCount;
+
+	RemoteProducerHandler(int producerId) {
+		super();
+		mProducerId = producerId;
+	}
+
+	@Override
+	public int getCount() {
+		return mCount;
+	}
+
+	@Override
+	protected void parseItems() throws XmlPullParserException, IOException {
+		if (mProducerId == 0) {
+			return;
+		}
+		Uri uri = createUri();
+		int type;
+		while ((type = mParser.next()) != END_DOCUMENT) {
+			if (type == START_TAG && ItemTag().equals(mParser.getName())) {
+				Cursor cursor = mResolver.query(uri, new String[] { idColumn() }, null, null, null);
+				try {
+					if (cursor.getCount() > 0) {
+						ContentValues values = parseProducer(uri);
+						mCount = mResolver.update(uri, values, null, null);
+					} else {
+						Log.w(TAG, "Tried to parse " + type() + ", but ID not in database: " + mProducerId);
+					}
+				} finally {
+					if (cursor != null) {
+						cursor.close();
+					}
+				}
+			}
+		}
+	}
+
+	private ContentValues parseProducer(Uri uri) throws XmlPullParserException, IOException {
+		final int depth = mParser.getDepth();
+		ContentValues values = new ContentValues();
+		String tag = null;
+
+		int type;
+		while (((type = mParser.next()) != END_TAG || mParser.getDepth() > depth) && type != END_DOCUMENT) {
+			if (type == START_TAG) {
+				tag = mParser.getName();
+			} else if (type == END_TAG) {
+				tag = null;
+			} else if (type == TEXT) {
+				String text = mParser.getText();
+				if (nameTag().equals(tag)) {
+					values.put(nameColumn(), text);
+				} else if (descriptionTag().equals(tag)) {
+					values.put(descriptionColumn(), text);
+				}
+			}
+		}
+		values.put(updatedColumn(), System.currentTimeMillis());
+		return values;
+	}
+
+	protected abstract String type();
+
+	protected abstract Uri createUri();
+
+	protected abstract String ItemTag();
+
+	protected abstract String nameTag();
+
+	protected abstract String descriptionTag();
+
+	protected abstract String idColumn();
+
+	protected abstract String nameColumn();
+
+	protected abstract String descriptionColumn();
+
+	protected abstract String updatedColumn();
+}
