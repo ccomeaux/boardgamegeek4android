f3ad1f204ee5b269efc62ad2587bb1201aff06cc
Chris Comeaux <chris.comeaux@gmail.com>
2012-05-14 02:03:05 +0000

    More handler refactoring

--- BoardGameGeek/src/com/boardgamegeek/io/RemoteDesignerHandler.java
+++ BoardGameGeek/src/com/boardgamegeek/io/RemoteDesignerHandler.java
@@ -1,100 +1,61 @@
 package com.boardgamegeek.io;
 
-import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
-import static org.xmlpull.v1.XmlPullParser.END_TAG;
-import static org.xmlpull.v1.XmlPullParser.START_TAG;
-import static org.xmlpull.v1.XmlPullParser.TEXT;
-
-import java.io.IOException;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
-import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.Designers;
 
-public class RemoteDesignerHandler extends XmlHandler {
-	private static final String TAG = "RemoteDesignerHandler";
-
-	private int mDesignerId;
-
-	public RemoteDesignerHandler() {
-		super(BggContract.CONTENT_AUTHORITY);
-	}
-
-	public RemoteDesignerHandler(int designerId) {
-		super(BggContract.CONTENT_AUTHORITY);
-		mDesignerId = designerId;
+public class RemoteDesignerHandler extends RemoteProducerHandler {
+	public RemoteDesignerHandler(int producerId) {
+		super(producerId);
 	}
 
 	@Override
-	public boolean parse(XmlPullParser parser, ContentResolver resolver, String authority)
-			throws XmlPullParserException, IOException {
-
-		if (mDesignerId == 0) {
-			return false;
-		}
-
-		String[] projection = { Designers.DESIGNER_ID };
-
-		int type;
-		while ((type = parser.next()) != END_DOCUMENT) {
-			if (type == START_TAG && Tags.PERSON.equals(parser.getName())) {
-
-				Uri uri = Designers.buildDesignerUri(mDesignerId);
-				Cursor cursor = resolver.query(uri, projection, null, null, null);
-
-				if (!cursor.moveToFirst()) {
-					Log.w(TAG, "Tried to parse designer, but ID not in database: " + mDesignerId);
-				} else {
-					parseDesigner(parser, resolver, uri);
-				}
-
-				cursor.close();
-			}
-		}
-
-		return false;
+	protected String type() {
+		return "designer";
 	}
 
-	private void parseDesigner(XmlPullParser parser, ContentResolver resolver, Uri uri) throws XmlPullParserException,
-			IOException {
-
-		final int depth = parser.getDepth();
-		ContentValues values = new ContentValues();
-		String tag = null;
-
-		int type;
-		while (((type = parser.next()) != END_TAG || parser.getDepth() > depth) && type != END_DOCUMENT) {
-			if (type == START_TAG) {
-				tag = parser.getName();
-			} else if (type == END_TAG) {
-				tag = null;
-			} else if (type == TEXT) {
-				String text = parser.getText();
-
-				if (Tags.NAME.equals(tag)) {
-					values.put(Designers.DESIGNER_NAME, text);
-				} else if (Tags.DESCRIPTION.equals(tag)) {
-					values.put(Designers.DESIGNER_DESCRIPTION, text);
-				}
-			}
-		}
-
-		values.put(Designers.UPDATED, System.currentTimeMillis());
-		resolver.update(uri, values, null, null);
+	@Override
+	protected Uri createUri() {
+		return Designers.buildDesignerUri(mProducerId);
 	}
 
-	private interface Tags {
-		// String PEOPLE = "people";
-		String PERSON = "person";
-		String NAME = "name";
-		String DESCRIPTION = "description";
+	@Override
+	protected String ItemTag() {
+		return "person";
+	}
+
+	@Override
+	protected String nameTag() {
+		return "name";
+	}
+
+	@Override
+	protected String descriptionTag() {
+		return "description";
+	}
+
+	@Override
+	protected String idColumn() {
+		return Designers.DESIGNER_ID;
+	}
+
+	@Override
+	protected String nameColumn() {
+		return Designers.DESIGNER_NAME;
+	}
+
+	@Override
+	protected String descriptionColumn() {
+		return Designers.DESIGNER_DESCRIPTION;
+	}
+
+	@Override
+	protected String updatedColumn() {
+		return Designers.UPDATED;
+	}
+
+	@Override
+	protected String getRootNodeName() {
+		return "people";
 	}
 }
--- BoardGameGeek/src/com/boardgamegeek/io/RemotePublisherHandler.java
+++ BoardGameGeek/src/com/boardgamegeek/io/RemotePublisherHandler.java
@@ -1,103 +1,61 @@
 package com.boardgamegeek.io;
 
-import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
-import static org.xmlpull.v1.XmlPullParser.END_TAG;
-import static org.xmlpull.v1.XmlPullParser.START_TAG;
-import static org.xmlpull.v1.XmlPullParser.TEXT;
-
-import java.io.IOException;
-
-import org.xmlpull.v1.XmlPullParserException;
-
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
 import com.boardgamegeek.provider.BggContract.Publishers;
 
-public class RemotePublisherHandler extends RemoteBggHandler {
-	private static final String TAG = "RemotePublisherHandler";
-
-	private int mPublisherId;
-	private int mCount;
-
-	public RemotePublisherHandler() {
-		super();
-	}
-
-	public RemotePublisherHandler(int publisherId) {
-		super();
-		mPublisherId = publisherId;
-	}
-
-	@Override
-	public int getCount() {
-		return mCount;
+public class RemotePublisherHandler extends RemoteProducerHandler {
+	public RemotePublisherHandler(int producerId) {
+		super(producerId);
 	}
 
 	@Override
 	protected String getRootNodeName() {
-		return Tags.COMPANIES;
+		return "companies";
 	}
 
 	@Override
-	protected void parseItems() throws XmlPullParserException, IOException {
-		if (mPublisherId == 0) {
-			return;
-		}
-		Uri uri = Publishers.buildPublisherUri(mPublisherId);
-
-		int type;
-		while ((type = mParser.next()) != END_DOCUMENT) {
-			if (type == START_TAG && Tags.COMPANY.equals(mParser.getName())) {
-
-				Cursor cursor = mResolver.query(uri, new String[] { Publishers.PUBLISHER_ID }, null, null, null);
-				try {
-					if (cursor.getCount() > 0) {
-						ContentValues values = parsePublisher(uri);
-						mCount = mResolver.update(uri, values, null, null);
-					} else {
-						Log.w(TAG, "Tried to parse publisher, but ID not in database: " + mPublisherId);
-					}
-				} finally {
-					if (cursor != null) {
-						cursor.close();
-					}
-				}
-			}
-		}
+	protected String type() {
+		return "publisher";
 	}
 
-	private ContentValues parsePublisher(Uri uri) throws XmlPullParserException, IOException {
-		final int depth = mParser.getDepth();
-		ContentValues values = new ContentValues();
-		String tag = null;
-
-		int type;
-		while (((type = mParser.next()) != END_TAG || mParser.getDepth() > depth) && type != END_DOCUMENT) {
-			if (type == START_TAG) {
-				tag = mParser.getName();
-			} else if (type == END_TAG) {
-				tag = null;
-			} else if (type == TEXT) {
-				String text = mParser.getText();
-
-				if (Tags.NAME.equals(tag)) {
-					values.put(Publishers.PUBLISHER_NAME, text);
-				} else if (Tags.DESCRIPTION.equals(tag)) {
-					values.put(Publishers.PUBLISHER_DESCRIPTION, text);
-				}
-			}
-		}
-		values.put(Publishers.UPDATED, System.currentTimeMillis());
-		return values;
+	@Override
+	protected Uri createUri() {
+		return Publishers.buildPublisherUri(mProducerId);
 	}
 
-	private interface Tags {
-		String COMPANIES = "companies";
-		String COMPANY = "company";
-		String NAME = "name";
-		String DESCRIPTION = "description";
+	@Override
+	protected String ItemTag() {
+		return "company";
+	}
+
+	@Override
+	protected String nameTag() {
+		return "name";
+	}
+
+	@Override
+	protected String descriptionTag() {
+		return "description";
+	}
+
+	@Override
+	protected String idColumn() {
+		return Publishers.PUBLISHER_ID;
+	}
+
+	@Override
+	protected String nameColumn() {
+		return Publishers.PUBLISHER_NAME;
+	}
+
+	@Override
+	protected String descriptionColumn() {
+		return Publishers.PUBLISHER_DESCRIPTION;
+	}
+
+	@Override
+	protected String updatedColumn() {
+		return Publishers.UPDATED;
 	}
 }
