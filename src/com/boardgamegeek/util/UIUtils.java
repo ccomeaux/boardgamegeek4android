a873bc8084f1278a93842a44cbb39778eeb74711
Chris Comeaux <chris.comeaux@gmail.com>
2012-09-02 22:12:47 +0000

    Move intent to fragment methods to UIUtils; remove unused layout

--- src/com/boardgamegeek/util/UIUtils.java
+++ src/com/boardgamegeek/util/UIUtils.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -258,6 +259,47 @@ public class UIUtils {
 				});
 			builder.create().show();
 		}
+	}
+
+	/**
+	 * Converts an intent into a {@link Bundle} suitable for use as fragment arguments.
+	 */
+	public static Bundle intentToFragmentArguments(Intent intent) {
+		Bundle arguments = new Bundle();
+		if (intent == null) {
+			return arguments;
+		}
+
+		final Uri data = intent.getData();
+		if (data != null) {
+			arguments.putParcelable("_uri", data);
+		}
+
+		final Bundle extras = intent.getExtras();
+		if (extras != null) {
+			arguments.putAll(intent.getExtras());
+		}
+
+		return arguments;
+	}
+
+	/**
+	 * Converts a fragment arguments bundle into an intent.
+	 */
+	public static Intent fragmentArgumentsToIntent(Bundle arguments) {
+		Intent intent = new Intent();
+		if (arguments == null) {
+			return intent;
+		}
+
+		final Uri data = arguments.getParcelable("_uri");
+		if (data != null) {
+			intent.setData(data);
+		}
+
+		intent.putExtras(arguments);
+		intent.removeExtra("_uri");
+		return intent;
 	}
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
