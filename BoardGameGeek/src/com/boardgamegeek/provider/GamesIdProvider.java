4891c9a2d85942f3b116ff1073f834e2760793d4
Chris Comeaux <chris.comeaux@gmail.com>
2012-09-04 01:01:07 +0000

    Include the game's rank when retrieving the game

--- BoardGameGeek/src/com/boardgamegeek/provider/GamesIdProvider.java
+++ BoardGameGeek/src/com/boardgamegeek/provider/GamesIdProvider.java
@@ -2,11 +2,20 @@ package com.boardgamegeek.provider;
 
 import android.net.Uri;
 
+import com.boardgamegeek.provider.BggContract.GameRanks;
 import com.boardgamegeek.provider.BggContract.Games;
+import com.boardgamegeek.provider.BggDatabase.Tables;
 import com.boardgamegeek.util.SelectionBuilder;
 
 public class GamesIdProvider extends BaseProvider {
 	final GamesProvider mProvider = new GamesProvider();
+
+	@Override
+	protected SelectionBuilder buildExpandedSelection(Uri uri) {
+		int gameId = Games.getGameId(uri);
+		return new SelectionBuilder().table(Tables.GAMES_JOIN_GAME_RANKS).mapToTable(Games.GAME_ID, Tables.GAMES)
+			.whereEquals(Tables.GAMES + "." + Games.GAME_ID, gameId).whereEquals(GameRanks.GAME_RANK_TYPE, "subtype");
+	}
 
 	@Override
 	protected SelectionBuilder buildSimpleSelection(Uri uri) {
