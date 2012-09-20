6f7a87317918c851a0c92c8e9286761852c2e42f
Chris Comeaux <chris.comeaux@gmail.com>
2012-09-20 00:39:16 +0000

    Move width and height from styles to layouts Fix artist, designer, etc. list width Add links section (pretty ugly, but works) Try to launch a new ImageActivity (there's a problem retrieving it from the disk cache) Attempt to fix launching a shortcut

--- src/com/boardgamegeek/ui/ImageActivity.java
+++ src/com/boardgamegeek/ui/ImageActivity.java
@@ -1,92 +1,19 @@
 package com.boardgamegeek.ui;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
 import com.boardgamegeek.R;
-import com.boardgamegeek.util.ImageUtils;
-import com.boardgamegeek.util.UIUtils;
 
-public class ImageActivity extends Activity {
-	// private final static String TAG = "ImageActivity";
-
-	public static final String KEY_GAME_NAME = "GAME_NAME";
+public class ImageActivity extends SimpleSinglePaneActivity {
 	public static final String KEY_IMAGE_URL = "IMAGE_URL";
-	public static final String KEY_THUMBNAIL_URL = "THUMBNAIL_URL";
-
-	private ImageView mImageView;
-	private String mImageUrl;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_image);
-		UIUtils.setTitle(this);
-		UIUtils.allowTypeToSearch(this);
-
-		mImageView = (ImageView) findViewById(R.id.image);
-
-		final Intent intent = getIntent();
-		mImageUrl = intent.getStringExtra(KEY_IMAGE_URL);
-
-		Drawable d = ImageUtils.getImageFromCache(this, intent.getStringExtra(KEY_THUMBNAIL_URL));
-		if (d != null) {
-			mImageView.setVisibility(View.VISIBLE);
-			mImageView.setImageDrawable(d);
-		}
-
-		String gameName = intent.getStringExtra(KEY_GAME_NAME);
-		((TextView) findViewById(R.id.game_name)).setText(gameName);
-
-		new ImageTask().execute(mImageUrl);
+	protected Fragment onCreatePane() {
+		return new ImageFragment();
 	}
 
 	@Override
-	public void setTitle(CharSequence title) {
-		UIUtils.setTitle(this, title);
-	}
-
-	public void onHomeClick(View v) {
-		UIUtils.resetToHome(this);
-	}
-
-	public void onSearchClick(View v) {
-		onSearchRequested();
-	}
-
-	private class ImageTask extends AsyncTask<String, Void, Drawable> {
-		private boolean mOomError = false;
-
-		@Override
-		protected void onPreExecute() {
-			findViewById(R.id.image_progress).setVisibility(View.VISIBLE);
-		}
-
-		@Override
-		protected Drawable doInBackground(String... params) {
-			try {
-				return ImageUtils.getImage(ImageActivity.this, params[0]);
-			} catch (OutOfMemoryError e) {
-				mOomError = true;
-			}
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Drawable result) {
-			findViewById(R.id.image_progress).setVisibility(View.GONE);
-			if (result != null) {
-				mImageView.setVisibility(View.VISIBLE);
-				mImageView.setImageDrawable(result);
-			} else if (mOomError) {
-				findViewById(R.id.image_error).setVisibility(View.VISIBLE);
-			}
-		}
+	protected int getOptionsMenuId() {
+		return R.menu.search_only;
 	}
 }
