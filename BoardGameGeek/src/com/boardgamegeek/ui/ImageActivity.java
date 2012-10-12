f07af537fd0d469d0576dda3027941f6f060d51d
Marek Jeszka <marek.jeszka@gmail.com>
2012-10-12 16:42:50 +0000

    Fixing FC error during navigation from ImageActivity with use of home button

--- BoardGameGeek/src/com/boardgamegeek/ui/ImageActivity.java
+++ BoardGameGeek/src/com/boardgamegeek/ui/ImageActivity.java
@@ -1,7 +1,9 @@
 package com.boardgamegeek.ui;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import com.actionbarsherlock.view.MenuItem;
 import com.boardgamegeek.R;
 
 public class ImageActivity extends SimpleSinglePaneActivity {
@@ -15,5 +17,16 @@ public class ImageActivity extends SimpleSinglePaneActivity {
 	@Override
 	protected int getOptionsMenuId() {
 		return R.menu.search_only;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				startActivity(new Intent(this, CollectionActivity.class));
+				finish();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
