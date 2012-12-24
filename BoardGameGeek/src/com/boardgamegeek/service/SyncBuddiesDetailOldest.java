4c27cb3bbe6b21fb6faa7d6651a60ba1f811a29c
Chris Comeaux <chris.comeaux@gmail.com>
2012-12-24 20:34:45 +0000

    Refactor syncing of buddies, including using the AccountManager to store last sync time

---
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncBuddiesDetailOldest.java
@@ -0,0 +1,50 @@
+package com.boardgamegeek.service;
+
+import static com.boardgamegeek.util.LogUtils.LOGI;
+import static com.boardgamegeek.util.LogUtils.makeLogTag;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.accounts.Account;
+import android.content.SyncResult;
+
+import com.boardgamegeek.BggApplication;
+import com.boardgamegeek.database.ResolverUtils;
+import com.boardgamegeek.io.RemoteBuddyUserHandler;
+import com.boardgamegeek.io.RemoteExecutor;
+import com.boardgamegeek.provider.BggContract.Buddies;
+import com.boardgamegeek.util.UserUrlBuilder;
+
+public class SyncBuddiesDetailOldest extends SyncTask {
+	private static final String TAG = makeLogTag(SyncBuddiesDetailOldest.class);
+	private static final int SYNC_LIMIT = 25;
+
+	@Override
+	public void execute(RemoteExecutor executor, Account account, SyncResult syncResult) throws IOException,
+		XmlPullParserException {
+		LOGI(TAG, "Syncing full collection list...");
+		try {
+			if (!BggApplication.getInstance().getSyncBuddies()) {
+				LOGI(TAG, "...buddies not set to sync");
+				return;
+			}
+
+			List<String> names = ResolverUtils.queryStrings(executor.getContext().getContentResolver(),
+				Buddies.CONTENT_URI, Buddies.BUDDY_NAME, null, null, Buddies.UPDATED + " LIMIT " + SYNC_LIMIT);
+			LOGI(TAG, "...found " + names.size() + " buddies to update");
+			if (names.size() > 0) {
+				for (String name : names) {
+					RemoteBuddyUserHandler handler = new RemoteBuddyUserHandler();
+					String url = new UserUrlBuilder(name).build();
+					executor.executeGet(url, handler);
+					syncResult.stats.numUpdates += handler.getCount();
+				}
+			}
+		} finally {
+			LOGI(TAG, "...complete!");
+		}
+	}
+}
---
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncBuddiesDetailUnupdated.java
@@ -0,0 +1,45 @@
+package com.boardgamegeek.service;
+
+import static com.boardgamegeek.util.LogUtils.LOGI;
+import static com.boardgamegeek.util.LogUtils.makeLogTag;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.accounts.Account;
+import android.content.SyncResult;
+
+import com.boardgamegeek.database.ResolverUtils;
+import com.boardgamegeek.io.RemoteBuddyUserHandler;
+import com.boardgamegeek.io.RemoteExecutor;
+import com.boardgamegeek.provider.BggContract.Buddies;
+import com.boardgamegeek.provider.BggContract.SyncColumns;
+import com.boardgamegeek.util.UserUrlBuilder;
+
+public class SyncBuddiesDetailUnupdated extends SyncTask {
+	private static final String TAG = makeLogTag(SyncBuddiesDetailUnupdated.class);
+
+	@Override
+	public void execute(RemoteExecutor executor, Account account, SyncResult syncResult) throws IOException,
+		XmlPullParserException {
+		LOGI(TAG, "Syncing unupdated buddies...");
+		try {
+			List<String> names = ResolverUtils.queryStrings(executor.getContext().getContentResolver(),
+				Buddies.CONTENT_URI, Buddies.BUDDY_NAME, SyncColumns.UPDATED + "=0 OR " + SyncColumns.UPDATED
+					+ " IS NULL", null);
+			LOGI(TAG, "...found " + names.size() + " buddies to update");
+			if (names.size() > 0) {
+				for (String name : names) {
+					RemoteBuddyUserHandler handler = new RemoteBuddyUserHandler();
+					String url = new UserUrlBuilder(name).build();
+					executor.executeGet(url, handler);
+					syncResult.stats.numUpdates += handler.getCount();
+				}
+			}
+		} finally {
+			LOGI(TAG, "...complete!");
+		}
+	}
+}
