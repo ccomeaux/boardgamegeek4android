931353113ac7ee9ec7114a1cee0178837ce0e0da
Chris Comeaux <chris.comeaux@gmail.com>
2013-01-14 22:25:29 +0000

    Prevent deleting collection when getting an IOException

--- BoardGameGeek/src/com/boardgamegeek/service/SyncCollectionListComplete.java
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncCollectionListComplete.java
@@ -13,13 +13,13 @@ import android.accounts.AccountManager;
 import android.content.SyncResult;
 import android.text.TextUtils;
 
-import com.boardgamegeek.BggApplication;
 import com.boardgamegeek.R;
 import com.boardgamegeek.io.RemoteCollectionHandler;
 import com.boardgamegeek.io.RemoteExecutor;
 import com.boardgamegeek.provider.BggContract.Collection;
 import com.boardgamegeek.util.CollectionUrlBuilder;
 import com.boardgamegeek.util.DateTimeUtils;
+import com.boardgamegeek.util.PreferencesUtils;
 
 public class SyncCollectionListComplete extends SyncTask {
 	private static final String TAG = makeLogTag(SyncCollectionListComplete.class);
@@ -28,8 +28,9 @@ public class SyncCollectionListComplete extends SyncTask {
 	public void execute(RemoteExecutor executor, Account account, SyncResult syncResult) throws IOException,
 		XmlPullParserException {
 		LOGI(TAG, "Syncing full collection list...");
+		boolean success = true;
 		try {
-			String[] statuses = BggApplication.getInstance().getSyncStatuses();
+			String[] statuses = PreferencesUtils.getSyncStatuses(executor.getContext());
 			if (statuses == null || statuses.length == 0) {
 				LOGI(TAG, "...no statuses set to sync");
 				return;
@@ -58,22 +59,27 @@ public class SyncCollectionListComplete extends SyncTask {
 					// This happens rather frequently with an EOF exception
 					LOGE(TAG, "  Problem syncing status [" + statuses[i] + "] (continuing with next status)", e);
 					syncResult.stats.numIoExceptions++;
+					success = false;
 				}
 			}
 
-			LOGI(TAG, "...deleting old collection entries");
-			// Delete all collection items that weren't updated in the sync above
-			int count = executor
-				.getContext()
-				.getContentResolver()
-				.delete(Collection.CONTENT_URI, Collection.UPDATED_LIST + "<?",
-					new String[] { String.valueOf(startTime) });
-			syncResult.stats.numDeletes += count;
-			// TODO: delete games as well?!
-			// TODO: delete thumbnail images associated with this list (both collection and game
+			if (success) {
+				LOGI(TAG, "...deleting old collection entries");
+				// Delete all collection items that weren't updated in the sync above
+				int count = executor
+					.getContext()
+					.getContentResolver()
+					.delete(Collection.CONTENT_URI, Collection.UPDATED_LIST + "<?",
+						new String[] { String.valueOf(startTime) });
+				syncResult.stats.numDeletes += count;
+				// TODO: delete games as well?!
+				// TODO: delete thumbnail images associated with this list (both collection and game
 
-			accountManager.setUserData(account, SyncService.TIMESTAMP_COLLECTION_COMPLETE, String.valueOf(startTime));
-			accountManager.setUserData(account, SyncService.TIMESTAMP_COLLECTION_PARTIAL, String.valueOf(startTime));
+				accountManager.setUserData(account, SyncService.TIMESTAMP_COLLECTION_COMPLETE,
+					String.valueOf(startTime));
+				accountManager
+					.setUserData(account, SyncService.TIMESTAMP_COLLECTION_PARTIAL, String.valueOf(startTime));
+			}
 		} finally {
 			LOGI(TAG, "...complete!");
 		}
