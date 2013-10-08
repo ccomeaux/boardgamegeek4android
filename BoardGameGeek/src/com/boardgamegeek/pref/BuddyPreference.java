5b01096aa048b6453189d6e26fe55d644593f46f
Chris Comeaux <chris.comeaux@medseek.com>
2013-10-08 13:31:23 -0500

    More refactoring

--- BoardGameGeek/src/com/boardgamegeek/pref/BuddyPreference.java
+++ BoardGameGeek/src/com/boardgamegeek/pref/BuddyPreference.java
@@ -7,25 +7,21 @@ import android.util.AttributeSet;
 import com.boardgamegeek.util.ActivityUtils;
 
 public class BuddyPreference extends Preference {
-	private Context mContext;
 
 	public BuddyPreference(Context context) {
 		super(context);
-		mContext = context;
 	}
 
 	public BuddyPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mContext = context;
 	}
 
 	public BuddyPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		mContext = context;
 	}
 
 	@Override
 	protected void onClick() {
-		ActivityUtils.link(mContext, "http://boardgamegeek.com/user/" + getSummary());
+		ActivityUtils.link(getContext(), "http://boardgamegeek.com/user/" + getSummary());
 	}
 }
