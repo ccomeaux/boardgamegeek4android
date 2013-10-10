6cfd18398b3694d02369bac965f7e5833c12bb00
Chris Comeaux <chris.comeaux@medseek.com>
2013-10-09 22:16:48 -0500

    Change preferences to use headers

--- BoardGameGeek/src/com/boardgamegeek/pref/ContactUsPreference.java
+++ BoardGameGeek/src/com/boardgamegeek/pref/ContactUsPreference.java
@@ -8,21 +8,16 @@ import android.preference.Preference;
 import android.util.AttributeSet;
 
 public class ContactUsPreference extends Preference {
-	private Context mContext;
-
 	public ContactUsPreference(Context context) {
 		super(context);
-		mContext = context;
 	}
 
 	public ContactUsPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mContext = context;
 	}
 
 	public ContactUsPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		mContext = context;
 	}
 
 	@Override
@@ -30,8 +25,8 @@ public class ContactUsPreference extends Preference {
 		Intent emailIntent = new Intent(Intent.ACTION_SEND);
 		emailIntent.setType("text/email");
 		emailIntent.putExtra(Intent.EXTRA_EMAIL,
-			new String[] { mContext.getString(R.string.pref_about_contact_us_summary) });
+			new String[] { getContext().getString(R.string.pref_about_contact_us_summary) });
 		emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-		mContext.startActivity(emailIntent);
+		getContext().startActivity(emailIntent);
 	}
 }
