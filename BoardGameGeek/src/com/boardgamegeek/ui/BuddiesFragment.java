c930da744c8d3df846863b3f0e4090b525efa718
Chris Comeaux <chris.comeaux@gmail.com>
2014-01-06 17:07:51 -0600

    Enable fast scrolling on buddy list

--- BoardGameGeek/src/com/boardgamegeek/ui/BuddiesFragment.java
+++ BoardGameGeek/src/com/boardgamegeek/ui/BuddiesFragment.java
@@ -67,6 +67,12 @@ public class BuddiesFragment extends BggListFragment implements LoaderManager.Lo
 	}
 
 	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		getListView().setFastScrollEnabled(true);
+	}
+
+	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setEmptyText(getString(R.string.empty_buddies));
