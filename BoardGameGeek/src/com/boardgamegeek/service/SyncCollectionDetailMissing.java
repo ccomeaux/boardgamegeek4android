eb0dce8cc12d172c503f329fb8f9e1c3aa98936e
Chris Comeaux <chris.comeaux@gmail.com>
2014-01-13 15:52:18 -0600

    Clean up the SyncCollectionDetailMissing task and set it to 3 days Update version description

--- BoardGameGeek/src/com/boardgamegeek/service/SyncCollectionDetailMissing.java
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncCollectionDetailMissing.java
@@ -11,6 +11,7 @@ import org.xmlpull.v1.XmlPullParserException;
 import android.accounts.Account;
 import android.content.ContentResolver;
 import android.content.SyncResult;
+import android.text.format.DateUtils;
 
 import com.boardgamegeek.R;
 import com.boardgamegeek.io.RemoteExecutor;
@@ -20,26 +21,36 @@ import com.boardgamegeek.util.ResolverUtils;
 
 public class SyncCollectionDetailMissing extends SyncTask {
 	private static final String TAG = makeLogTag(SyncCollectionDetailMissing.class);
-	private static final int HOURS_OLD = 0;
+	private static final int HOURS_OLD = 72;
 
 	@Override
 	public void execute(RemoteExecutor executor, Account account, SyncResult syncResult) throws IOException,
 		XmlPullParserException {
-		LOGI(TAG, "Removing missing games from the collection...");
+		LOGI(TAG, "Deleting missing games from the collection...");
 		try {
-			String arg = String.valueOf(DateTimeUtils.hoursAgo(HOURS_OLD));
-			LOGI(TAG, "...since " + arg);
+			long hoursAgo = DateTimeUtils.hoursAgo(HOURS_OLD);
+			String arg = String.valueOf(hoursAgo);
+			LOGI(
+				TAG,
+				"...since "
+					+ DateUtils.formatDateTime(executor.getContext(), hoursAgo, DateUtils.FORMAT_SHOW_DATE
+						| DateUtils.FORMAT_NUMERIC_DATE | DateUtils.FORMAT_SHOW_TIME));
 			ContentResolver resolver = executor.getContext().getContentResolver();
-			List<Integer> gameIds = ResolverUtils.queryInts(resolver, Games.CONTENT_URI, Games.GAME_ID,
-				"collection.game_id IS NULL AND games.updated_list<?", new String[] { arg }, "games." + Games.UPDATED);
-			LOGI(TAG, "...found " + gameIds.size() + " games to remove");
+			List<Integer> gameIds = ResolverUtils
+				.queryInts(resolver, Games.CONTENT_URI, Games.GAME_ID,
+					"collection.game_id IS NULL AND games.updated_list < ?", new String[] { arg }, "games."
+						+ Games.UPDATED);
+			LOGI(TAG, "...found " + gameIds.size() + " games to delete");
 			if (gameIds.size() > 0) {
+				int count = 0;
 				for (Integer gameId : gameIds) {
-					LOGI(TAG, "Deleting game ID=" + gameId);
-					resolver.delete(Games.buildGameUri(gameId), null, null);
+					LOGI(TAG, "...deleting game ID=" + gameId);
+					count += resolver.delete(Games.buildGameUri(gameId), null, null);
 				}
-				// syncResult.stats.numDeletes += gameIds.size();
+				LOGI(TAG, "...deleted " + count + " games");
+				// syncResult.stats.numDeletes += count;
 			}
+			// NOTE: We're not deleting on with the selection because it doesn't perform the game/collection join
 		} finally {
 			LOGI(TAG, "...complete!");
 		}
