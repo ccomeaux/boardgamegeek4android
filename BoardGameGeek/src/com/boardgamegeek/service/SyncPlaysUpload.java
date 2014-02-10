399f8c0601be7111456a2be4ce0fabd3ff34a7bc
Chris Comeaux <chris.comeaux@gmail.com>
2014-02-09 22:41:50 -0600

    In the play logged/updated/deleted notification, display the game name in bold instead of quotes

--- BoardGameGeek/src/com/boardgamegeek/service/SyncPlaysUpload.java
+++ BoardGameGeek/src/com/boardgamegeek/service/SyncPlaysUpload.java
@@ -32,7 +32,10 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.TextUtils;
+import android.text.style.StyleSpan;
 
 import com.boardgamegeek.BggApplication;
 import com.boardgamegeek.R;
@@ -58,7 +61,7 @@ public class SyncPlaysUpload extends SyncTask {
 
 	private Context mContext;
 	private HttpClient mClient;
-	private List<String> mMessages;
+	private List<CharSequence> mMessages;
 	private LocalBroadcastManager mBroadcaster;
 
 	@Override
@@ -66,7 +69,7 @@ public class SyncPlaysUpload extends SyncTask {
 		XmlPullParserException {
 		mContext = executor.getContext();
 		mClient = executor.getHttpClient();
-		mMessages = new ArrayList<String>();
+		mMessages = new ArrayList<CharSequence>();
 		mBroadcaster = LocalBroadcastManager.getInstance(mContext);
 
 		updatePendingPlays(account.name, syncResult);
@@ -101,11 +104,10 @@ public class SyncPlaysUpload extends SyncTask {
 							deletePlay(play, syncResult);
 						}
 
-						Resources r = mContext.getResources();
-						String message = String.format(
-							r.getString(play.hasBeenSynced() ? R.string.msg_play_updated : R.string.msg_play_added),
-							getPlayCountDescription(response.count, play.Quantity), play.GameName);
-						notifyUser(message);
+						String message = play.hasBeenSynced() ? mContext.getString(R.string.msg_play_updated)
+							: mContext.getString(R.string.msg_play_added,
+								getPlayCountDescription(response.count, play.Quantity));
+						notifyUser(message, play.GameName);
 					} else {
 						notifyUser(error);
 					}
@@ -144,14 +146,14 @@ public class SyncPlaysUpload extends SyncTask {
 					if (TextUtils.isEmpty(error)) {
 						decreaseGamePlayCount(play);
 						PlayPersister.delete(mContext.getContentResolver(), play);
-						notifyUser(String.format(mContext.getString(R.string.msg_play_deleted), play.GameName));
+						notifyUser(R.string.msg_play_deleted, play.GameName);
 						// syncResult.stats.numDeletes++;
 					} else {
 						notifyUser(error);
 					}
 				} else {
 					PlayPersister.delete(mContext.getContentResolver(), play);
-					notifyUser(String.format(mContext.getString(R.string.msg_play_deleted_draft), play.GameName));
+					notifyUser(R.string.msg_play_deleted_draft, play.GameName);
 					// syncResult.stats.numDeletes++;
 				}
 			}
@@ -391,7 +393,20 @@ public class SyncPlaysUpload extends SyncTask {
 		return playCount;
 	}
 
-	private void notifyUser(String message) {
+	private void notifyUser(int messageId, String gameName) {
+		notifyUser(mContext.getString(messageId), gameName);
+	}
+
+	private void notifyUser(String message, String gameName) {
+		String formattableMessage = message + " " + gameName;
+		SpannableString ss = new SpannableString(formattableMessage);
+		int length = message.length() + 1;
+		ss.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), length, length + gameName.length(),
+			Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		notifyUser(ss);
+	}
+
+	private void notifyUser(CharSequence message) {
 		mMessages.add(message);
 
 		NotificationCompat.Builder builder = createNotificationBuilder().setContentText(message);
