010f58b532f1a544c0120aa51f7345823bccbf38
Chris Comeaux <chris.comeaux@gmail.com>
2014-12-20 07:37:55 -0600

    Add icons to the nav drawer and change the highlight

--- app/src/main/java/com/boardgamegeek/ui/DrawerActivity.java
+++ app/src/main/java/com/boardgamegeek/ui/DrawerActivity.java
@@ -6,8 +6,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
@@ -75,28 +77,28 @@ public abstract class DrawerActivity extends BaseActivity {
 
 		mDrawerList.addView(makeNavDrawerSeparator(R.string.title_my_geek, mDrawerList));
 		if (!Authenticator.isSignedIn(DrawerActivity.this)) {
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_signin, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_signin, R.drawable.ic_account_circle_black_24dp, mDrawerList));
 		} else {
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_collection, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_collection, R.drawable.ic_my_library_books_black_24dp, mDrawerList));
 			mDrawerList.addView(makeNavDrawerSpacer(mDrawerList));
 			mDrawerList.addView(makeNavDrawerDivider(mDrawerList));
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_plays, mDrawerList));
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_players, mDrawerList));
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_locations, mDrawerList));
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_play_stats, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_plays, R.drawable.ic_event_note_black_24dp, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_players, R.drawable.ic_people_black_24dp, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_locations, R.drawable.ic_place_black_24dp, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_play_stats, R.drawable.ic_poll_black_24dp, mDrawerList));
 			mDrawerList.addView(makeNavDrawerSpacer(mDrawerList));
 			mDrawerList.addView(makeNavDrawerDivider(mDrawerList));
-			mDrawerList.addView(makeNavDrawerItem(R.string.title_buddies, mDrawerList));
+			mDrawerList.addView(makeNavDrawerItem(R.string.title_buddies, R.drawable.ic_person_black_24dp, mDrawerList));
 			mDrawerList.addView(makeNavDrawerSpacer(mDrawerList));
 		}
 
 		mDrawerList.addView(makeNavDrawerSeparator(R.string.title_browse, mDrawerList));
-		mDrawerList.addView(makeNavDrawerItem(R.string.title_hotness, mDrawerList));
-		mDrawerList.addView(makeNavDrawerItem(R.string.title_geeklists, mDrawerList));
-		mDrawerList.addView(makeNavDrawerItem(R.string.title_forums, mDrawerList));
+		mDrawerList.addView(makeNavDrawerItem(R.string.title_hotness, R.drawable.ic_whatshot_black_24dp, mDrawerList));
+		mDrawerList.addView(makeNavDrawerItem(R.string.title_geeklists, R.drawable.ic_list_black_24dp, mDrawerList));
+		mDrawerList.addView(makeNavDrawerItem(R.string.title_forums, R.drawable.ic_forum_black_24dp, mDrawerList));
 		mDrawerList.addView(makeNavDrawerSpacer(mDrawerList));
 		mDrawerList.addView(makeNavDrawerDivider(mDrawerList));
-		mDrawerList.addView(makeNavDrawerItem(R.string.title_settings, mDrawerList));
+		mDrawerList.addView(makeNavDrawerItem(R.string.title_settings, R.drawable.ic_settings_black_24dp, mDrawerList));
 	}
 
 	private void selectItem(int titleResId) {
@@ -154,7 +156,7 @@ public abstract class DrawerActivity extends BaseActivity {
 	}
 
 	private View makeNavDrawerSeparator(final int titleId, ViewGroup container) {
-		int layoutToInflate = R.layout.row_header;
+		int layoutToInflate = R.layout.row_header_drawer;
 		View view = getLayoutInflater().inflate(layoutToInflate, container, false);
 		if (titleId != 0) {
 			TextView titleView = (TextView) view.findViewById(android.R.id.title);
@@ -163,17 +165,25 @@ public abstract class DrawerActivity extends BaseActivity {
 		return view;
 	}
 
-	private View makeNavDrawerItem(final int titleId, ViewGroup container) {
-		return makeNavDrawerItem(R.layout.row_drawer, titleId, container);
-	}
-
-	private View makeNavDrawerItem(int layoutId, final int titleId, ViewGroup container) {
-		View view = getLayoutInflater().inflate(layoutId, container, false);
+	private View makeNavDrawerItem(final int titleId, int iconId, ViewGroup container) {
+		View view = getLayoutInflater().inflate(R.layout.row_drawer, container, false);
 
 		TextView titleView = (TextView) view.findViewById(android.R.id.title);
+		ImageView iconView = (ImageView) view.findViewById(android.R.id.icon);
+
 		titleView.setText(titleId);
+		if (iconId != 0) {
+			iconView.setImageResource(iconId);
+			iconView.setVisibility(View.VISIBLE);
+		} else {
+			iconView.setVisibility(View.GONE);
+		}
 		if (titleId == getDrawerResId()) {
+			view.setBackgroundResource(R.color.background_light);
 			titleView.setTextColor(getResources().getColor(R.color.background_dark));
+			iconView.setColorFilter(getResources().getColor(R.color.background_dark));
+		} else {
+			iconView.setColorFilter(getResources().getColor(R.color.navdrawer_icon_tint));
 		}
 
 		view.setOnClickListener(new View.OnClickListener() {
