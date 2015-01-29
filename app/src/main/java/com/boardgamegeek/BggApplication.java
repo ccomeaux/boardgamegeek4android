0a1fe63f2b5a63898c442d490f72f17e9bbff630
Chris Comeaux <chris.comeaux@gmail.com>
2015-01-28 21:38:24 -0600

    Add Fabric/Crashlytics to app and add simple hook

--- app/src/main/java/com/boardgamegeek/BggApplication.java
+++ app/src/main/java/com/boardgamegeek/BggApplication.java
@@ -2,19 +2,24 @@ package com.boardgamegeek;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 import hugo.weaving.DebugLog;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 import static timber.log.Timber.DebugTree;
 
 public class BggApplication extends Application {
-
 	@Override
 	@DebugLog
 	public void onCreate() {
+		super.onCreate();
 		if (BuildConfig.DEBUG) {
 			Timber.plant(new DebugTree());
 		} else {
+			Fabric.with(this, new Crashlytics());
+			//TODO: replace with Crashlytics tree
 			Timber.plant(new Timber.HollowTree());
 		}
 	}
