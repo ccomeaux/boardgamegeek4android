33b936877ef8b3d0a2255af381dfa5d0aa4c0b9d
Chris Comeaux <chris.comeaux@gmail.com>
2015-02-09 18:41:52 -0600

    Improve clickable nature of GameDetailFragment row

--- app/src/main/java/com/boardgamegeek/ui/GameDetailFragment.java
+++ app/src/main/java/com/boardgamegeek/ui/GameDetailFragment.java
@@ -31,14 +31,7 @@ public class GameDetailFragment extends BggListFragment implements LoaderManager
 	private CursorAdapter mAdapter;
 	private int mGameId;
 	private int mQueryToken;
-	private Query mQuery;
-
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		final ListView listView = getListView();
-		listView.setSelector(android.R.color.transparent);
-	}
+	private BaseQuery mQuery;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -50,6 +43,11 @@ public class GameDetailFragment extends BggListFragment implements LoaderManager
 		mQueryToken = intent.getIntExtra(ActivityUtils.KEY_QUERY_TOKEN, BggContract.INVALID_ID);
 
 		determineQuery();
+
+		if (!mQuery.isClickable()) {
+			getListView().setSelector(android.R.color.transparent);
+		}
+
 		if (mQueryToken != BggContract.INVALID_ID) {
 			getLoaderManager().restartLoader(mQueryToken, getArguments(), this);
 		} else {
@@ -162,6 +160,10 @@ public class GameDetailFragment extends BggListFragment implements LoaderManager
 		public Uri getUri(Cursor cursor) {
 			return null;
 		}
+
+		protected boolean isClickable() {
+			return true;
+		}
 	}
 
 	private class DesignerQuery extends BaseQuery {
@@ -253,6 +255,11 @@ public class GameDetailFragment extends BggListFragment implements LoaderManager
 		public Uri getUri() {
 			return Games.buildCategoriesUri(mGameId);
 		}
+
+		@Override
+		protected boolean isClickable() {
+			return false;
+		}
 	}
 
 	private class MechanicQuery extends BaseQuery {
@@ -271,6 +278,11 @@ public class GameDetailFragment extends BggListFragment implements LoaderManager
 		@Override
 		public Uri getUri() {
 			return Games.buildMechanicsUri(mGameId);
+		}
+
+		@Override
+		protected boolean isClickable() {
+			return false;
 		}
 	}
 
