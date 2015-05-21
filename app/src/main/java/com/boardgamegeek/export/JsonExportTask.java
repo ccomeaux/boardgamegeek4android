ed711de68dc77b59c46a3877f649865a9892a193
Chris Comeaux <chris.comeaux@gmail.com>
2015-05-21 07:50:22 -0500

    Export/import player colors

--- app/src/main/java/com/boardgamegeek/export/JsonExportTask.java
+++ app/src/main/java/com/boardgamegeek/export/JsonExportTask.java
@@ -26,126 +26,127 @@ import de.greenrobot.event.EventBus;
 import timber.log.Timber;
 
 public class JsonExportTask extends AsyncTask<Void, Integer, Integer> {
-	private static final int SUCCESS = 1;
-	private static final int ERROR = 0;
-	private static final int ERROR_STORAGE_ACCESS = -1;
+    private static final int SUCCESS = 1;
+    private static final int ERROR = 0;
+    private static final int ERROR_STORAGE_ACCESS = -1;
 
-	private final Context mContext;
-	private final boolean mIsAutoBackupMode;
+    private final Context mContext;
+    private final boolean mIsAutoBackupMode;
 
-	public JsonExportTask(Context context, boolean isAutoBackupMode) {
-		mContext = context.getApplicationContext();
-		mIsAutoBackupMode = isAutoBackupMode;
-	}
+    public JsonExportTask(Context context, boolean isAutoBackupMode) {
+        mContext = context.getApplicationContext();
+        mIsAutoBackupMode = isAutoBackupMode;
+    }
 
-	@Override
-	protected Integer doInBackground(Void... params) {
-		// Ensure external storage is available
-		if (!FileUtils.isExtStorageAvailable()) {
-			return ERROR_STORAGE_ACCESS;
-		}
+    @Override
+    protected Integer doInBackground(Void... params) {
+        // Ensure external storage is available
+        if (!FileUtils.isExtStorageAvailable()) {
+            return ERROR_STORAGE_ACCESS;
+        }
 
-		// Ensure the export directory exists
-		File exportPath = FileUtils.getExportPath(mIsAutoBackupMode);
-		if (!exportPath.exists()) {
-			if (!exportPath.mkdirs()) {
-				return ERROR_STORAGE_ACCESS;
-			}
-		}
+        // Ensure the export directory exists
+        File exportPath = FileUtils.getExportPath(mIsAutoBackupMode);
+        if (!exportPath.exists()) {
+            if (!exportPath.mkdirs()) {
+                return ERROR_STORAGE_ACCESS;
+            }
+        }
 
-		List<ImporterExporter> exporters = new ArrayList<>();
-		exporters.add(new CollectionViewImporterExporter());
-		exporters.add(new GameImporterExporter());
+        List<ImporterExporter> exporters = new ArrayList<>();
+        exporters.add(new CollectionViewImporterExporter());
+        exporters.add(new GameImporterExporter());
+        exporters.add(new UserImporterExporter());
 
-		for (ImporterExporter exporter : exporters) {
-			int result = export(exportPath, exporter);
-			if (result == ERROR || isCancelled()) {
-				return ERROR;
-			}
-		}
+        for (ImporterExporter exporter : exporters) {
+            int result = export(exportPath, exporter);
+            if (result == ERROR || isCancelled()) {
+                return ERROR;
+            }
+        }
 
-		//TODO: auto-backup
-		//		if (mIsAutoBackupMode) {
-		//			// store current time = last backup time
-		//			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-		//			prefs.edit().putLong(KEY_LASTBACKUP, System.currentTimeMillis()).commit();
-		//		}
+        //TODO: auto-backup
+        //		if (mIsAutoBackupMode) {
+        //			// store current time = last backup time
+        //			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        //			prefs.edit().putLong(KEY_LASTBACKUP, System.currentTimeMillis()).commit();
+        //		}
 
-		return SUCCESS;
-	}
+        return SUCCESS;
+    }
 
-	@Override
-	protected void onProgressUpdate(Integer... values) {
-		EventBus.getDefault().post(new ExportProgressEvent(values[0], values[1]));
-	}
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        EventBus.getDefault().post(new ExportProgressEvent(values[0], values[1]));
+    }
 
-	@Override
-	protected void onPostExecute(Integer result) {
-		if (!mIsAutoBackupMode) {
-			int messageId;
-			switch (result) {
-				case SUCCESS:
-					messageId = R.string.pref_advanced_export_msg_success;
-					break;
-				case ERROR_STORAGE_ACCESS:
-					messageId = R.string.pref_advanced_export_msg_failed_nosd;
-					break;
-				default:
-					messageId = R.string.pref_advanced_export_msg_failed;
-					break;
-			}
-			Toast.makeText(mContext, messageId, Toast.LENGTH_LONG).show();
-		}
+    @Override
+    protected void onPostExecute(Integer result) {
+        if (!mIsAutoBackupMode) {
+            int messageId;
+            switch (result) {
+                case SUCCESS:
+                    messageId = R.string.pref_advanced_export_msg_success;
+                    break;
+                case ERROR_STORAGE_ACCESS:
+                    messageId = R.string.pref_advanced_export_msg_failed_nosd;
+                    break;
+                default:
+                    messageId = R.string.pref_advanced_export_msg_failed;
+                    break;
+            }
+            Toast.makeText(mContext, messageId, Toast.LENGTH_LONG).show();
+        }
 
-		EventBus.getDefault().post(new ExportFinishedEvent());
-	}
+        EventBus.getDefault().post(new ExportFinishedEvent());
+    }
 
-	private int export(File exportPath, ImporterExporter exporter) {
-		final Cursor cursor = exporter.getCursor(mContext);
+    private int export(File exportPath, ImporterExporter exporter) {
+        final Cursor cursor = exporter.getCursor(mContext);
 
-		if (cursor == null) {
-			return ERROR;
-		}
-		if (cursor.getCount() == 0) {
-			cursor.close();
-			return SUCCESS;
-		}
+        if (cursor == null) {
+            return ERROR;
+        }
+        if (cursor.getCount() == 0) {
+            cursor.close();
+            return SUCCESS;
+        }
 
-		publishProgress(cursor.getCount(), 0);
+        publishProgress(cursor.getCount(), 0);
 
-		File backup = new File(exportPath, exporter.getFileName());
-		try {
-			OutputStream out = new FileOutputStream(backup);
-			writeJsonStream(out, cursor, exporter);
-		} catch (JsonIOException | IOException e) {
-			Timber.e(e, "JSON export failed");
-			return ERROR;
-		} finally {
-			cursor.close();
-		}
+        File backup = new File(exportPath, exporter.getFileName());
+        try {
+            OutputStream out = new FileOutputStream(backup);
+            writeJsonStream(out, cursor, exporter);
+        } catch (JsonIOException | IOException e) {
+            Timber.e(e, "JSON export failed");
+            return ERROR;
+        } finally {
+            cursor.close();
+        }
 
-		return SUCCESS;
-	}
+        return SUCCESS;
+    }
 
-	private void writeJsonStream(OutputStream out, Cursor cursor, ImporterExporter exporter) throws IOException {
-		int numTotal = cursor.getCount();
-		int numExported = 0;
+    private void writeJsonStream(OutputStream out, Cursor cursor, ImporterExporter exporter) throws IOException {
+        int numTotal = cursor.getCount();
+        int numExported = 0;
 
-		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-		JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
-		writer.setIndent("  ");
-		writer.beginArray();
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+        writer.setIndent("  ");
+        writer.beginArray();
 
-		while (cursor.moveToNext()) {
-			if (isCancelled()) {
-				break;
-			}
+        while (cursor.moveToNext()) {
+            if (isCancelled()) {
+                break;
+            }
 
-			exporter.writeJsonRecord(mContext, cursor, gson, writer);
-			publishProgress(numTotal, ++numExported);
-		}
+            exporter.writeJsonRecord(mContext, cursor, gson, writer);
+            publishProgress(numTotal, ++numExported);
+        }
 
-		writer.endArray();
-		writer.close();
-	}
+        writer.endArray();
+        writer.close();
+    }
 }
