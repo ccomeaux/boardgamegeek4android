9a237a1c11cc9a1a39496ceb418f7d18e13d2a1b
Chris Comeaux <chris.comeaux@gmail.com>
2015-08-02 15:11:22 -0500

    Syncing plays now retries with an exponential back-off.

---
+++ app/src/main/java/com/boardgamegeek/io/RetryableRequest.java
@@ -0,0 +1,60 @@
+package com.boardgamegeek.io;
+
+import timber.log.Timber;
+
+public abstract class RetryableRequest<T> {
+
+	private static final long MIN_WAIT_TIME = 100L;
+	private static final long MAX_WAIT_TIME = 60000L;
+	private static final int MAX_RETRIES = 10;
+	protected final BggService mService;
+
+	public RetryableRequest(BggService service) {
+		mService = service;
+	}
+
+	public T execute() {
+		int retries = 0;
+		do {
+			try {
+				return request();
+			} catch (Exception e) {
+				if (e.getCause() instanceof RetryableException) {
+					Timber.w(e, "Retryable exception");
+					retries++;
+					long waitTime = getWaitTime(retries);
+					Timber.i("...retry #" + retries + " in " + waitTime + "ms");
+					try {
+						Thread.sleep(waitTime);
+					} catch (InterruptedException e1) {
+						Timber.e(e1, "Interrupted while sleeping during retry.");
+					}
+				} else {
+					Timber.e(e, "Syncing plays");
+					throw e;
+				}
+			}
+		} while (retries < getMaxRetries());
+		Timber.w("Exceeded maximum retries");
+		throw new RuntimeException("Can't sync plays due to service error.");
+	}
+
+	protected abstract T request();
+
+	protected static long getWaitTime(int retryCount) {
+		long waitTime = ((long) Math.pow(2, retryCount) * getMinWaitTime());
+		return Math.min(MAX_WAIT_TIME, waitTime);
+	}
+
+	protected static long getMinWaitTime() {
+		return MIN_WAIT_TIME;
+	}
+
+	protected static long getMaxWaitTime() {
+		return MAX_WAIT_TIME;
+	}
+
+	protected static int getMaxRetries() {
+		return MAX_RETRIES;
+	}
+}
