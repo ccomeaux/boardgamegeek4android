8da6b2a7584aa16de895172eb839fc2991112326
Chris Comeaux <chris.comeaux@gmail.com>
2015-10-06 00:13:32 -0500

    Export using runtime permissions

--- app/src/main/java/com/boardgamegeek/ui/DataFragment.java
+++ app/src/main/java/com/boardgamegeek/ui/DataFragment.java
@@ -1,11 +1,15 @@
 package com.boardgamegeek.ui;
 
+import android.Manifest.permission;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +32,7 @@ import de.greenrobot.event.EventBus;
 import hugo.weaving.DebugLog;
 
 public class DataFragment extends Fragment {
+	private static final int REQUEST_EXPORT = 0;
 	@SuppressWarnings("unused") @InjectView(R.id.backup_location) TextView mFileLocationView;
 	@SuppressWarnings("unused") @InjectView(R.id.backup_types) ViewGroup mFileTypes;
 
@@ -72,9 +77,41 @@ public class DataFragment extends Fragment {
 		DialogUtils.createConfirmationDialog(getActivity(), R.string.msg_export_confirmation, new OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				TaskUtils.executeAsyncTask(new JsonExportTask(getContext(), false));
+				if (PackageManager.PERMISSION_GRANTED ==
+					ContextCompat.checkSelfPermission(getActivity(), permission.WRITE_EXTERNAL_STORAGE)) {
+					export();
+				} else {
+					if (shouldShowRequestPermissionRationale(permission.WRITE_EXTERNAL_STORAGE)) {
+						View v = getView();
+						if (v != null) {
+							Snackbar.make(v, R.string.msg_export_permission_rationale, Snackbar.LENGTH_INDEFINITE).show();
+						}
+					}
+					requestPermissions(new String[] { permission.WRITE_EXTERNAL_STORAGE }, REQUEST_EXPORT);
+				}
 			}
 		}).show();
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode == REQUEST_EXPORT) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				export();
+			} else {
+				View v = getView();
+				if (v != null) {
+					Snackbar.make(v, R.string.msg_export_permission_denied, Snackbar.LENGTH_LONG).show();
+				}
+			}
+		} else {
+			super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+
+	@DebugLog
+	private void export() {
+		TaskUtils.executeAsyncTask(new JsonExportTask(getContext(), false));
 	}
 
 	@DebugLog
