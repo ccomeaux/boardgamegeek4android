a531069ccf6fe506f2542a3ebc390ebaf3bc5c8e
Chris Comeaux <chris.comeaux@gmail.com>
2015-12-05 15:42:35 -0600

    Rename CollectionConvertor to CollectionPostConvertor

--- app/src/main/java/com/boardgamegeek/io/CollectionPostConverter.java
+++ app/src/main/java/com/boardgamegeek/io/CollectionPostConverter.java
@@ -10,10 +10,10 @@ import java.lang.reflect.Type;
 import retrofit.converter.ConversionException;
 import retrofit.mime.TypedInput;
 
-public class CollectionConverter extends PostConverter {
+public class CollectionPostConverter extends PostConverter {
 	private static final String CLASS_NAME = "class com.boardgamegeek.model.CollectionPostResponse";
 
-	public CollectionConverter() {
+	public CollectionPostConverter() {
 	}
 
 	@Override
