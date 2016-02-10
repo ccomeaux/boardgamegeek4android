d67ba18d46eb9170bf66f7bd3bc094aa2835807a
Chris Comeaux <chris.comeaux@gmail.com>
2016-02-09 19:33:10 -0600

    Refactor the SliderFilter

--- app/src/main/java/com/boardgamegeek/ui/dialog/SliderFilter.java
+++ app/src/main/java/com/boardgamegeek/ui/dialog/SliderFilter.java
@@ -2,6 +2,7 @@ package com.boardgamegeek.ui.dialog;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,12 @@ import com.boardgamegeek.ui.widget.RangeSeekBar;
 import com.boardgamegeek.ui.widget.RangeSeekBar.OnRangeSeekBarChangeListener;
 
 public abstract class SliderFilter {
-	private Integer mMin;
-	private Integer mMax;
-	private TextView mDescriptionView;
-	private TextView mTextInterval;
-	private RangeSeekBar<Integer> mRangeSeekBar;
-	private CheckBox mCheckBox;
+	private Integer low;
+	private Integer high;
+	private TextView explanationView;
+	private TextView rangeDescriptionView;
+	private RangeSeekBar<Integer> rangeSeekBar;
+	private CheckBox checkBox;
 
 	public void createDialog(final Context context, final CollectionView view, CollectionFilterer filter) {
 		initValues(filter);
@@ -29,20 +30,20 @@ public abstract class SliderFilter {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View layout = inflater.inflate(R.layout.dialog_slider_filter, null);
 
-		mDescriptionView = (TextView) layout.findViewById(R.id.slider_filter_description);
-		mTextInterval = (TextView) layout.findViewById(R.id.slider_filter_text);
-		mCheckBox = (CheckBox) layout.findViewById(R.id.slider_filter_checkbox);
-		FrameLayout container = (FrameLayout) layout.findViewById(R.id.slider_filter_rangeseekbar_container);
-		mRangeSeekBar = new RangeSeekBar<>(getAbsoluteMin(), getAbsoluteMax(), context);
-		container.addView(mRangeSeekBar);
+		rangeDescriptionView = (TextView) layout.findViewById(R.id.range_description);
+		checkBox = (CheckBox) layout.findViewById(R.id.checkbox);
+		explanationView = (TextView) layout.findViewById(R.id.explanation);
+		FrameLayout container = (FrameLayout) layout.findViewById(R.id.range_seek_bar_container);
+		rangeSeekBar = new RangeSeekBar<>(getAbsoluteMin(), getAbsoluteMax(), context);
+		container.addView(rangeSeekBar);
 
-		mMin = getMin();
-		mMax = getMax();
+		low = getMin();
+		high = getMax();
 
-		initDescription();
 		initSlider();
 		initCheckbox();
-		mTextInterval.setText(intervalText(getMin(), getMax()));
+		initExplanation();
+		rangeDescriptionView.setText(intervalText(low, high));
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(context).setTitle(getTitleId())
 			.setNegativeButton(R.string.clear, new DialogInterface.OnClickListener() {
@@ -53,7 +54,7 @@ public abstract class SliderFilter {
 			}).setPositiveButton(R.string.set, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int id) {
-					captureForm(mMin, mMax, mCheckBox.isChecked());
+					captureForm(low, high, checkBox.isChecked());
 					view.addFilter(getPositiveData(context));
 				}
 			}).setView(layout);
@@ -61,45 +62,46 @@ public abstract class SliderFilter {
 		builder.create().show();
 	}
 
-	private void initDescription() {
-		if (getDescriptionId() == -1) {
-			mDescriptionView.setVisibility(View.GONE);
-		} else {
-			mDescriptionView.setText(getDescriptionId());
-			mDescriptionView.setVisibility(View.VISIBLE);
-		}
-	}
-
 	private void initSlider() {
-		mRangeSeekBar.setNotifyWhileDragging(true);
-		mRangeSeekBar.setSelectedMinValue(getMin());
-		mRangeSeekBar.setSelectedMaxValue(getMax());
+		rangeSeekBar.setNotifyWhileDragging(true);
+		rangeSeekBar.setSelectedMinValue(low);
+		rangeSeekBar.setSelectedMaxValue(high);
 
-		mRangeSeekBar.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Integer>() {
+		rangeSeekBar.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Integer>() {
 			@Override
 			public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
-				mMin = minValue;
-				mMax = maxValue;
+				low = minValue;
+				high = maxValue;
 				CharSequence text;
 				if (minValue.equals(maxValue)) {
 					text = intervalText(minValue);
 				} else {
 					text = intervalText(minValue, maxValue);
 				}
-				mTextInterval.setText(text);
+				rangeDescriptionView.setText(text);
 			}
 		});
 	}
 
 	private void initCheckbox() {
 		//noinspection ResourceType
-		mCheckBox.setVisibility(getCheckboxVisibility());
-		mCheckBox.setText(getCheckboxTextId());
-		mCheckBox.setChecked(isChecked());
+		checkBox.setVisibility(getCheckboxVisibility());
+		checkBox.setText(getCheckboxTextId());
+		checkBox.setChecked(isChecked());
+	}
+
+	private void initExplanation() {
+		if (getDescriptionId() == -1) {
+			explanationView.setVisibility(View.GONE);
+		} else {
+			explanationView.setText(getDescriptionId());
+			explanationView.setVisibility(View.VISIBLE);
+		}
 	}
 
 	protected abstract void initValues(CollectionFilterer filter);
 
+	@StringRes
 	protected abstract int getTitleId();
 
 	protected abstract CollectionFilterer getNegativeData();
@@ -116,10 +118,12 @@ public abstract class SliderFilter {
 		return View.VISIBLE;
 	}
 
+	@StringRes
 	protected int getCheckboxTextId() {
 		return R.string.include_missing_values;
 	}
 
+	@StringRes
 	protected int getDescriptionId() {
 		return -1;
 	}
