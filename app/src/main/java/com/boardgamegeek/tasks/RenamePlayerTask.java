cb087faa5a88919f9910d0a64e47f79f83b7e245
Chris Comeaux <chris.comeaux@gmail.com>
2016-03-01 21:49:25 -0600

    Update colors when renaming a player

--- app/src/main/java/com/boardgamegeek/tasks/RenamePlayerTask.java
+++ app/src/main/java/com/boardgamegeek/tasks/RenamePlayerTask.java
@@ -3,6 +3,7 @@ package com.boardgamegeek.tasks;
 import android.content.ContentProviderOperation;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -12,6 +13,7 @@ import com.boardgamegeek.R;
 import com.boardgamegeek.model.Play;
 import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.PlayPlayers;
+import com.boardgamegeek.provider.BggContract.PlayerColors;
 import com.boardgamegeek.provider.BggContract.Plays;
 import com.boardgamegeek.service.SyncService;
 import com.boardgamegeek.util.ResolverUtils;
@@ -20,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.greenrobot.event.EventBus;
+import timber.log.Timber;
 
 /**
  * Change a player name (either a GeekBuddy or named player), updating and syncing all plays.
@@ -48,6 +51,17 @@ public class RenamePlayerTask extends AsyncTask<Void, Void, String> {
 		ArrayList<ContentProviderOperation> batch = new ArrayList<>();
 		String selection = TextUtils.isEmpty(username) ? SELECTION_WITHOUT_USERNAME : SELECTION_WITH_USERNAME;
 
+		updatePlays(batch, selection);
+		updatePlayers(batch, selection);
+		updateColors(batch);
+		ResolverUtils.applyBatch(context, batch);
+
+		SyncService.sync(context, SyncService.FLAG_SYNC_PLAYS_UPLOAD);
+
+		return context.getString(R.string.msg_play_player_change, oldName, newName);
+	}
+
+	private void updatePlays(ArrayList<ContentProviderOperation> batch, String selection) {
 		List<Integer> playIds = ResolverUtils.queryInts(context.getContentResolver(),
 			Plays.buildPlayersByPlayUri(),
 			Plays.PLAY_ID, Plays.SYNC_STATUS + "=? AND (" + selection + ")",
@@ -64,19 +78,40 @@ public class RenamePlayerTask extends AsyncTask<Void, Void, String> {
 				}
 			}
 		}
+	}
 
+	private void updatePlayers(ArrayList<ContentProviderOperation> batch, String selection) {
 		batch.add(ContentProviderOperation
 			.newUpdate(Plays.buildPlayersByPlayUri())
 			.withValue(PlayPlayers.NAME, newName)
 			.withSelection(selection,
 				TextUtils.isEmpty(username) ? new String[] { oldName, "" } : new String[] { oldName, username })
 			.build());
+	}
 
-		ResolverUtils.applyBatch(context, batch);
-
-		SyncService.sync(context, SyncService.FLAG_SYNC_PLAYS_UPLOAD);
-
-		return context.getString(R.string.msg_play_player_change, oldName, newName);
+	private void updateColors(ArrayList<ContentProviderOperation> batch) {
+		Cursor cursor = context.getContentResolver().query(PlayerColors.buildPlayerUri(oldName),
+			new String[] { PlayerColors.PLAYER_COLOR, PlayerColors.PLAYER_COLOR_SORT_ORDER },
+			null, null, null);
+		try {
+			if (cursor != null) {
+				while (cursor.moveToNext()) {
+					batch.add(ContentProviderOperation
+						.newInsert(PlayerColors.buildPlayerUri(newName))
+						.withValue(PlayerColors.PLAYER_COLOR, cursor.getString(0))
+						.withValue(PlayerColors.PLAYER_COLOR_SORT_ORDER, cursor.getInt(1))
+						.build());
+				}
+				Timber.i("Updated %,d colors", cursor.getCount());
+			} else {
+				Timber.i("No colors to update");
+			}
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+		batch.add(ContentProviderOperation.newDelete(PlayerColors.buildPlayerUri(oldName)).build());
 	}
 
 	@Override
