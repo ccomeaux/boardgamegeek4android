540a6cd6bc3e5a0003e09bd51a097aa7bd1b4e56
Chris Comeaux <chris.comeaux@gmail.com>
2016-03-06 14:57:26 -0600

    Add a menu item to add a username to a player missing one

--- app/src/main/java/com/boardgamegeek/ui/BuddyActivity.java
+++ app/src/main/java/com/boardgamegeek/ui/BuddyActivity.java
@@ -7,7 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.boardgamegeek.R;
 import com.boardgamegeek.events.BuddySelectedEvent;
 import com.boardgamegeek.tasks.BuddyNicknameUpdateTask;
 import com.boardgamegeek.tasks.RenamePlayerTask;
@@ -17,6 +20,8 @@ import de.greenrobot.event.EventBus;
 import hugo.weaving.DebugLog;
 
 public class BuddyActivity extends SimpleSinglePaneActivity {
+	private String username;
+
 	@Override
 	@DebugLog
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class BuddyActivity extends SimpleSinglePaneActivity {
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
 		final String name = getIntent().getStringExtra(ActivityUtils.KEY_PLAYER_NAME);
-		final String username = getIntent().getStringExtra(ActivityUtils.KEY_BUDDY_NAME);
+		username = getIntent().getStringExtra(ActivityUtils.KEY_BUDDY_NAME);
 		setSubtitle(username, name);
 
 		EventBus.getDefault().removeStickyEvent(BuddySelectedEvent.class);
@@ -36,6 +41,29 @@ public class BuddyActivity extends SimpleSinglePaneActivity {
 	@DebugLog
 	protected Fragment onCreatePane(Intent intent) {
 		return new BuddyFragment();
+	}
+
+	@Override
+	protected int getOptionsMenuId() {
+		return R.menu.buddy;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem item = menu.findItem(R.id.add_username);
+		if (item != null) {
+			item.setVisible(TextUtils.isEmpty(username));
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.add_username) {
+			showSnackbar("Adding username...");
+			return true;
+		}
+		return false;
 	}
 
 	@SuppressWarnings("unused")
@@ -76,5 +104,4 @@ public class BuddyActivity extends SimpleSinglePaneActivity {
 			Snackbar.make(rootContainer, message, Snackbar.LENGTH_LONG).show();
 		}
 	}
-
 }
