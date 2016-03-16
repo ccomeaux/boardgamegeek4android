7913c35499ed6aa342f7c3d4cbe158e6a216e235
Chris Comeaux <chris.comeaux@gmail.com>
2016-03-16 08:48:51 -0500

    Refactor update collection item tasks into a base class

--- app/src/main/java/com/boardgamegeek/tasks/UpdateCollectionItemCommentTask.java
+++ app/src/main/java/com/boardgamegeek/tasks/UpdateCollectionItemCommentTask.java
@@ -4,29 +4,22 @@ package com.boardgamegeek.tasks;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import com.boardgamegeek.events.CollectionItemUpdatedEvent;
 import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.Collection;
-import com.boardgamegeek.util.ResolverUtils;
 
 import de.greenrobot.event.EventBus;
 import hugo.weaving.DebugLog;
 import timber.log.Timber;
 
-public class UpdateCollectionItemCommentTask extends AsyncTask<Void, Void, Void> {
-	private final Context context;
-	private final int gameId;
-	private final int collectionId;
+public class UpdateCollectionItemCommentTask extends UpdateCollectionItemTask {
 	private final String comment;
 
 	@DebugLog
 	public UpdateCollectionItemCommentTask(Context context, int gameId, int collectionId, String comment) {
-		this.context = context;
-		this.gameId = gameId;
-		this.collectionId = collectionId;
+		super(context, gameId, collectionId);
 		this.comment = comment;
 	}
 
@@ -54,26 +47,5 @@ public class UpdateCollectionItemCommentTask extends AsyncTask<Void, Void, Void>
 	protected void onPostExecute(Void result) {
 		Timber.i("Updated game ID %1$s, collection ID %2$s with comment %3$s", gameId, collectionId, comment);
 		EventBus.getDefault().post(new CollectionItemUpdatedEvent());
-	}
-
-	@DebugLog
-	private static long getCollectionItemInternalId(ContentResolver resolver, int collectionId, int gameId) {
-		long internalId;
-		if (collectionId == BggContract.INVALID_ID) {
-			internalId = ResolverUtils.queryLong(resolver,
-				Collection.CONTENT_URI,
-				Collection._ID,
-				BggContract.INVALID_ID,
-				"collection." + Collection.GAME_ID + "=? AND " + Collection.COLLECTION_ID + " IS NULL",
-				new String[] { String.valueOf(gameId) });
-		} else {
-			internalId = ResolverUtils.queryLong(resolver,
-				Collection.CONTENT_URI,
-				Collection._ID,
-				BggContract.INVALID_ID,
-				Collection.COLLECTION_ID + "=?",
-				new String[] { String.valueOf(collectionId) });
-		}
-		return internalId;
 	}
 }
--- app/src/main/java/com/boardgamegeek/tasks/UpdateCollectionItemRatingTask.java
+++ app/src/main/java/com/boardgamegeek/tasks/UpdateCollectionItemRatingTask.java
@@ -1,32 +1,24 @@
 package com.boardgamegeek.tasks;
 
-
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import com.boardgamegeek.events.CollectionItemUpdatedEvent;
 import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.Collection;
-import com.boardgamegeek.util.ResolverUtils;
 
 import de.greenrobot.event.EventBus;
 import hugo.weaving.DebugLog;
 import timber.log.Timber;
 
-public class UpdateCollectionItemRatingTask extends AsyncTask<Void, Void, Void> {
-	private final Context context;
-	private final int gameId;
-	private final int collectionId;
+public class UpdateCollectionItemRatingTask extends UpdateCollectionItemTask {
 	private final double rating;
 
 	@DebugLog
 	public UpdateCollectionItemRatingTask(Context context, int gameId, int collectionId, double rating) {
-		this.context = context;
-		this.gameId = gameId;
-		this.collectionId = collectionId;
+		super(context, gameId, collectionId);
 		this.rating = rating;
 	}
 
@@ -54,26 +46,5 @@ public class UpdateCollectionItemRatingTask extends AsyncTask<Void, Void, Void> 
 	protected void onPostExecute(Void result) {
 		Timber.i("Updated game ID %1$s, collection ID %2$s with rating %3$s", gameId, collectionId, rating);
 		EventBus.getDefault().post(new CollectionItemUpdatedEvent());
-	}
-
-	@DebugLog
-	private long getCollectionItemInternalId(ContentResolver resolver, int collectionId, int gameId) {
-		long internalId;
-		if (collectionId == BggContract.INVALID_ID) {
-			internalId = ResolverUtils.queryLong(resolver,
-				Collection.CONTENT_URI,
-				Collection._ID,
-				BggContract.INVALID_ID,
-				"collection." + Collection.GAME_ID + "=? AND " + Collection.COLLECTION_ID + " IS NULL",
-				new String[] { String.valueOf(gameId) });
-		} else {
-			internalId = ResolverUtils.queryLong(resolver,
-				Collection.CONTENT_URI,
-				Collection._ID,
-				BggContract.INVALID_ID,
-				Collection.COLLECTION_ID + "=?",
-				new String[] { String.valueOf(collectionId) });
-		}
-		return internalId;
 	}
 }
