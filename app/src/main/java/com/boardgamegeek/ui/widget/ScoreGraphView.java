213d9d2db4e72c855ac1505d414735003f67c26b
Chris Comeaux <chris.comeaux@gmail.com>
2016-07-09 20:21:30 -0500

    Draw ticks every 10 points

--- app/src/main/java/com/boardgamegeek/ui/widget/ScoreGraphView.java
+++ app/src/main/java/com/boardgamegeek/ui/widget/ScoreGraphView.java
@@ -15,16 +15,18 @@ import com.boardgamegeek.R;
 public class ScoreGraphView extends View {
 	private Paint barPaint;
 	private Paint scorePaint;
+	private float scoreRadius;
+	private float smallTickHeight;
+	private float largeTickHeight;
+	private int lowScoreColor;
+	private int averageScoreColor;
+	private int averageWinScoreColor;
+	private int highScoreColor;
 
 	private double lowScore;
 	private double averageScore;
 	private double averageWinScore;
 	private double highScore;
-	private float scoreRadius;
-	private int lowScoreColor;
-	private int averageScoreColor;
-	private int averageWinScoreColor;
-	private int highScoreColor;
 
 	public ScoreGraphView(Context context) {
 		super(context);
@@ -44,7 +46,9 @@ public class ScoreGraphView extends View {
 		scorePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		scorePaint.setStrokeWidth(1);
 
-		scoreRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, context.getResources().getDisplayMetrics());
+		scoreRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6, context.getResources().getDisplayMetrics());
+		smallTickHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, context.getResources().getDisplayMetrics());
+		largeTickHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6, context.getResources().getDisplayMetrics());
 
 		lowScoreColor = ContextCompat.getColor(getContext(), R.color.score_low);
 		averageScoreColor = ContextCompat.getColor(getContext(), R.color.score_average);
@@ -78,6 +82,16 @@ public class ScoreGraphView extends View {
 
 		canvas.drawLine(left, y, right, y, barPaint);
 
+		// ticks
+		double tickScore = Math.ceil(lowScore / 10) * 10;
+		while (tickScore <= highScore) {
+			float x = (float) ((tickScore - lowScore) / (highScore - lowScore) * (right - left) + left);
+			final float tickHeight = (tickScore % 50) == 0 ? largeTickHeight : smallTickHeight;
+			canvas.drawLine(x, y - tickHeight, x, y + tickHeight, barPaint);
+			tickScore += 10;
+		}
+
+		// score dots
 		drawScore(canvas, y, left, right, lowScore, lowScoreColor);
 		drawScore(canvas, y, left, right, averageScore, averageScoreColor);
 		drawScore(canvas, y, left, right, averageWinScore, averageWinScoreColor);
