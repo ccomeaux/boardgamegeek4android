7ec32cd69ed0b6a4574573dc7e24231d6574de0c
Chris Comeaux <chris.comeaux@gmail.com>
2016-09-09 22:07:13 -0500

    Restyle the thread list

--- app/src/main/java/com/boardgamegeek/ui/adapter/ThreadRecyclerViewAdapter.java
+++ app/src/main/java/com/boardgamegeek/ui/adapter/ThreadRecyclerViewAdapter.java
@@ -66,7 +66,6 @@ public class ThreadRecyclerViewAdapter extends RecyclerView.Adapter<ThreadRecycl
 			if (article == null) return;
 
 			usernameView.setText(article.username);
-			editDateView.setFormat(article.getNumberOfEdits() > 0 ? R.string.edited_prefix : R.string.posted_prefix);
 			editDateView.setTimestamp(article.editDate());
 			UIUtils.setTextMaybeHtml(bodyView, article.body);
 			Bundle bundle = new Bundle();
