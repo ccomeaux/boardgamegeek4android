8694e41de73f4d221109e365a925eb6efcc4aa89
Chris Comeaux <chris.comeaux@gmail.com>
2017-02-02 20:55:24 -0600

    When syncing a game’s plays, delete the ones not found on the ‘Geek (and not dirty)

--- app/src/main/java/com/boardgamegeek/service/SyncGamePlays.java
+++ app/src/main/java/com/boardgamegeek/service/SyncGamePlays.java
@@ -13,6 +13,8 @@ import com.boardgamegeek.model.PlaysResponse;
 import com.boardgamegeek.model.persister.PlayPersister;
 import com.boardgamegeek.provider.BggContract;
 import com.boardgamegeek.provider.BggContract.Games;
+import com.boardgamegeek.provider.BggContract.Plays;
+import com.boardgamegeek.util.SelectionBuilder;
 
 import hugo.weaving.DebugLog;
 import timber.log.Timber;
@@ -60,6 +62,7 @@ public class SyncGamePlays extends UpdateTask {
 				persister.save(response.plays, startTime);
 				page++;
 			} while (response.hasMorePages());
+			deleteUnupdatedPlays(context, startTime);
 			updateGameTimestamp(context);
 			SyncService.hIndex(context);
 			Timber.i("Synced plays for game id=" + gameId);
@@ -74,5 +77,16 @@ public class SyncGamePlays extends UpdateTask {
 		ContentValues values = new ContentValues(1);
 		values.put(Games.UPDATED_PLAYS, System.currentTimeMillis());
 		context.getContentResolver().update(Games.buildGameUri(gameId), values, null, null);
+	}
+
+	private void deleteUnupdatedPlays(@NonNull Context context, long startTime) {
+		int count = context.getContentResolver().delete(Plays.CONTENT_URI,
+			Plays.SYNC_TIMESTAMP + "<? AND " +
+				Plays.OBJECT_ID + "=? AND " +
+				SelectionBuilder.whereZeroOrNull(Plays.UPDATE_TIMESTAMP) + " AND " +
+				SelectionBuilder.whereZeroOrNull(Plays.DELETE_TIMESTAMP) + " AND " +
+				SelectionBuilder.whereZeroOrNull(Plays.DIRTY_TIMESTAMP),
+			new String[] { String.valueOf(startTime), String.valueOf(gameId) });
+		Timber.i("Deleted %,d unupdated play(s) of game ID=%s", count, gameId);
 	}
 }
