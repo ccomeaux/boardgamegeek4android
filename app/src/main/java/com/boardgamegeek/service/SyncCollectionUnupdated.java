d76dbe768800169c145b02b3d477d9278c96442e
Chris Comeaux <chris.comeaux@gmail.com>
2017-02-11 16:55:02 -0600

    Improve notification text when syncing un-updated collection

--- app/src/main/java/com/boardgamegeek/service/SyncCollectionUnupdated.java
+++ app/src/main/java/com/boardgamegeek/service/SyncCollectionUnupdated.java
@@ -3,6 +3,7 @@ package com.boardgamegeek.service;
 import android.accounts.Account;
 import android.content.Context;
 import android.content.SyncResult;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v4.util.ArrayMap;
 import android.text.TextUtils;
@@ -13,9 +14,9 @@ import com.boardgamegeek.io.CollectionRequest;
 import com.boardgamegeek.io.CollectionResponse;
 import com.boardgamegeek.model.persister.CollectionPersister;
 import com.boardgamegeek.provider.BggContract.Collection;
-import com.boardgamegeek.util.ResolverUtils;
 import com.boardgamegeek.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import timber.log.Timber;
@@ -52,22 +53,16 @@ public class SyncCollectionUnupdated extends SyncTask {
 				if (isCancelled()) break;
 
 				numberOfFetches++;
-				List<Integer> gameIds = ResolverUtils.queryInts(context.getContentResolver(),
-					Collection.CONTENT_URI,
-					Collection.GAME_ID,
-					"(collection." + Collection.UPDATED + "=0 OR collection." + Collection.UPDATED + " IS NULL) AND " + Collection.COLLECTION_ID + " IS NOT NULL",
-					null,
-					"collection." + Collection.UPDATED_LIST + " DESC LIMIT " + GAME_PER_FETCH);
-				if (gameIds.size() > 0) {
-					String gameIdDescription = StringUtils.formatList(gameIds);
-					Timber.i("...found " + gameIds.size() + " games to update [" + gameIdDescription + "]");
-					String detail = gameIds.size() + " games: " + gameIdDescription;
+				GameList gameIds = queryGames();
+				if (gameIds.getSize() > 0) {
+					String detail = context.getString(R.string.sync_notification_collection_update_games, gameIds.getSize(), gameIds.getDescription());
 					if (numberOfFetches > 1) {
-						detail += " (page " + numberOfFetches + ")";
+						detail = context.getString(R.string.sync_notification_page_suffix, detail, numberOfFetches);
 					}
 					updateProgressNotification(detail);
+					Timber.i("...found %,d games to update [%s]", gameIds.getSize(), gameIds.getDescription());
 
-					options.put(BggService.COLLECTION_QUERY_KEY_ID, TextUtils.join(",", gameIds));
+					options.put(BggService.COLLECTION_QUERY_KEY_ID, gameIds.getIds());
 					options.remove(BggService.COLLECTION_QUERY_KEY_SUBTYPE);
 					boolean success = requestAndPersist(account.name, persister, options, syncResult);
 
@@ -92,6 +87,51 @@ public class SyncCollectionUnupdated extends SyncTask {
 			Timber.i("...complete!");
 		}
 	}
+
+	private static class GameList {
+		private final List<Integer> gameIds;
+		private final List<String> gameNames;
+
+		public GameList(int count) {
+			gameIds = new ArrayList<>(count);
+			gameNames = new ArrayList<>(count);
+		}
+
+		public void addGame(int id, String name) {
+			gameIds.add(id);
+			gameNames.add(name);
+		}
+
+		public int getSize() {
+			return gameIds.size();
+		}
+
+		public String getDescription() {
+			return StringUtils.formatList(gameNames);
+		}
+
+		public String getIds() {
+			return TextUtils.join(",", gameIds);
+		}
+	}
+
+	public GameList queryGames() {
+		GameList list = new GameList(GAME_PER_FETCH);
+		Cursor cursor = context.getContentResolver().query(Collection.CONTENT_URI,
+			new String[] { Collection.GAME_ID, Collection.GAME_NAME },
+			"(collection." + Collection.UPDATED + "=0 OR collection." + Collection.UPDATED + " IS NULL) AND " + Collection.COLLECTION_ID + " IS NOT NULL",
+			null,
+			"collection." + Collection.UPDATED_LIST + " DESC LIMIT " + GAME_PER_FETCH);
+		try {
+			while (cursor != null && cursor.moveToNext()) {
+				list.addGame(cursor.getInt(0), cursor.getString(1));
+			}
+		} finally {
+			if (cursor != null) cursor.close();
+		}
+		return list;
+	}
+
 
 	private boolean requestAndPersist(String username, @NonNull CollectionPersister persister, ArrayMap<String, String> options, @NonNull SyncResult syncResult) {
 		Timber.i("..requesting collection items with options %s", options);
