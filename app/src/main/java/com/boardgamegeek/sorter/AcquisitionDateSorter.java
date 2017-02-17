62ff6f3ce424f16fb6a444037522e9e75cbd0896
Chris Comeaux <chris.comeaux@gmail.com>
2017-02-17 14:34:48 -0600

    Refactor collection date sorters into a base class

--- app/src/main/java/com/boardgamegeek/sorter/AcquisitionDateSorter.java
+++ app/src/main/java/com/boardgamegeek/sorter/AcquisitionDateSorter.java
@@ -1,27 +1,15 @@
 package com.boardgamegeek.sorter;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
-import android.text.format.DateUtils;
 
 import com.boardgamegeek.R;
 import com.boardgamegeek.provider.BggContract.Collection;
-import com.boardgamegeek.util.DateTimeUtils;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-public class AcquisitionDateSorter extends CollectionSorter {
-	private static final String COLUMN_NAME = Collection.PRIVATE_INFO_ACQUISITION_DATE;
-	private static final SimpleDateFormat API_FORMAT = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-	private static final SimpleDateFormat DISPLAY_FORMAT = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
-	@NonNull private final String defaultValue;
-
+public class AcquisitionDateSorter extends CollectionDateSorter {
 	public AcquisitionDateSorter(@NonNull Context context) {
 		super(context);
-		defaultValue = context.getString(R.string.text_unknown);
 	}
 
 	@StringRes
@@ -38,35 +26,6 @@ public class AcquisitionDateSorter extends CollectionSorter {
 
 	@Override
 	protected String getSortColumn() {
-		return COLUMN_NAME;
-	}
-
-	@Override
-	protected boolean isSortDescending() {
-		return true;
-	}
-
-	@Override
-	public String getHeaderText(@NonNull Cursor cursor) {
-		long time = getTime(cursor);
-		if (time == DateTimeUtils.UNKNOWN_DATE) {
-			return defaultValue;
-		}
-		return DISPLAY_FORMAT.format(time);
-	}
-
-	@NonNull
-	@Override
-	public String getDisplayInfo(@NonNull Cursor cursor) {
-		long time = getTime(cursor);
-		if (time == DateTimeUtils.UNKNOWN_DATE) {
-			return defaultValue;
-		}
-		return DateUtils.getRelativeTimeSpanString(time, System.currentTimeMillis(), DateUtils.DAY_IN_MILLIS).toString();
-	}
-
-	private long getTime(@NonNull Cursor cursor) {
-		String date = getString(cursor, Collection.PRIVATE_INFO_ACQUISITION_DATE, defaultValue);
-		return DateTimeUtils.tryParseDate(DateTimeUtils.UNPARSED_DATE, date, API_FORMAT);
+		return Collection.PRIVATE_INFO_ACQUISITION_DATE;
 	}
 }
--- app/src/main/java/com/boardgamegeek/sorter/LastPlayDateSorter.java
+++ app/src/main/java/com/boardgamegeek/sorter/LastPlayDateSorter.java
@@ -1,27 +1,15 @@
 package com.boardgamegeek.sorter;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
-import android.text.format.DateUtils;
 
 import com.boardgamegeek.R;
 import com.boardgamegeek.provider.BggContract.Plays;
-import com.boardgamegeek.util.DateTimeUtils;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-public class LastPlayDateSorter extends CollectionSorter {
-	private static final SimpleDateFormat API_FORMAT = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-	private static final SimpleDateFormat DISPLAY_FORMAT = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
-	@NonNull private final String defaultValue;
-	private final String COLUMN_NAME = Plays.MAX_DATE;
-
+public class LastPlayDateSorter extends CollectionDateSorter {
 	public LastPlayDateSorter(@NonNull Context context) {
 		super(context);
-		defaultValue = context.getString(R.string.text_unknown);
 	}
 
 	@StringRes
@@ -32,37 +20,12 @@ public class LastPlayDateSorter extends CollectionSorter {
 
 	@Override
 	protected String getSortColumn() {
-		return COLUMN_NAME;
-	}
-
-	@Override
-	protected boolean isSortDescending() {
-		return true;
+		return Plays.MAX_DATE;
 	}
 
 	@StringRes
 	@Override
 	public int getTypeResource() {
 		return R.string.collection_sort_type_play_date_max;
-	}
-
-	@Override
-	public String getHeaderText(@NonNull Cursor cursor) {
-		long time = getTime(cursor);
-		if (time == DateTimeUtils.UNKNOWN_DATE) return defaultValue;
-		return DISPLAY_FORMAT.format(time);
-	}
-
-	@NonNull
-	@Override
-	public String getDisplayInfo(@NonNull Cursor cursor) {
-		long time = getTime(cursor);
-		if (time == DateTimeUtils.UNKNOWN_DATE) return defaultValue;
-		return DateUtils.getRelativeTimeSpanString(time, System.currentTimeMillis(), DateUtils.DAY_IN_MILLIS).toString();
-	}
-
-	private long getTime(@NonNull Cursor cursor) {
-		String date = getString(cursor, COLUMN_NAME, defaultValue);
-		return DateTimeUtils.tryParseDate(DateTimeUtils.UNPARSED_DATE, date, API_FORMAT);
 	}
 }
