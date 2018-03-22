9d96dc3c9fc7a14c4a7fe99fd65d1b822cd004df
Chris Comeaux <chris.comeaux@gmail.com>
2018-03-22 08:58:15 -0500

    Add search menu option to hotness activity

--- app/src/main/java/com/boardgamegeek/ui/HotnessActivity.kt
+++ app/src/main/java/com/boardgamegeek/ui/HotnessActivity.kt
@@ -1,9 +1,12 @@
 package com.boardgamegeek.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.boardgamegeek.R
 import com.crashlytics.android.answers.Answers
 import com.crashlytics.android.answers.ContentViewEvent
+import org.jetbrains.anko.intentFor
+
 
 class HotnessActivity : TopLevelSinglePaneActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,4 +19,15 @@ class HotnessActivity : TopLevelSinglePaneActivity() {
     override fun onCreatePane() = HotnessFragment()
 
     override fun getDrawerResId() = R.string.title_hotness
+
+    override val optionsMenuId = R.menu.search
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.menu_search) {
+            startActivity(intentFor<SearchResultsActivity>())
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
