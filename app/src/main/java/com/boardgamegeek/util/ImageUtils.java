abeaacc5aae47d2e53b07ee5d6e37c70cb95ca55
Chris Comeaux <chris.comeaux@gmail.com>
2018-04-11 14:26:12 -0500

    When loading a thumbnail by image ID, use the Geekdo API to get the URL

--- app/src/main/java/com/boardgamegeek/util/ImageUtils.java
+++ app/src/main/java/com/boardgamegeek/util/ImageUtils.java
@@ -2,15 +2,21 @@ package com.boardgamegeek.util;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
 import android.support.v7.graphics.Palette;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.boardgamegeek.R;
+import com.boardgamegeek.io.Adapter;
+import com.boardgamegeek.io.model.Image;
 import com.squareup.picasso.Picasso;
 
 import java.util.LinkedList;
 import java.util.Queue;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Loading images? This is your huckleberry.
@@ -25,20 +31,6 @@ public class ImageUtils {
 	//private static final String SUFFIX_LARGE = "_lg";
 
 	private ImageUtils() {
-	}
-
-	/**
-	 * Create a URL for a thumbnail image as a JPG.
-	 */
-	public static String createThumbnailJpgUrl(int imageId) {
-		return IMAGE_URL_PREFIX + imageId + SUFFIX_SMALL + ".jpg";
-	}
-
-	/**
-	 * Create a URL for a thumbnail image as a PNG.
-	 */
-	public static String createThumbnailPngUrl(int imageId) {
-		return IMAGE_URL_PREFIX + imageId + SUFFIX_SMALL + ".png";
 	}
 
 	/**
@@ -77,62 +69,13 @@ public class ImageUtils {
 		safelyLoadImage(imageView, imageUrls, callback);
 	}
 
-	/**
-	 * Loads an image into the {@link android.widget.ImageView} by attempting each URL in the {@link java.util.Queue}
-	 * until one is successful. Applies fit/center crop and will load a {@link android.support.v7.graphics.Palette}.
-	 */
-	private static void safelyLoadImage(final ImageView imageView,
-										final Queue<String> imageUrls,
-										final Callback callback) {
-		String savedUrl = (String) imageView.getTag(R.id.url);
-		String url = null;
-		if (!TextUtils.isEmpty(savedUrl)) {
-			if (imageUrls.contains(savedUrl)) {
-				url = savedUrl;
-			} else {
-				imageView.setTag(R.id.url, null);
-			}
-		}
-		if (TextUtils.isEmpty(url)) {
-			url = imageUrls.poll();
-		}
-		if (TextUtils.isEmpty(url)) {
-			if (callback != null) callback.onFailedImageLoad();
-			return;
-		}
-		final String imageUrl = url;
-		Picasso
-			.with(imageView.getContext())
-			.load(HttpUtils.ensureScheme(imageUrl))
-			.transform(PaletteTransformation.instance())
-			.into(imageView, new com.squareup.picasso.Callback() {
-				@Override
-				public void onSuccess() {
-					imageView.setTag(R.id.url, imageUrl);
-					if (callback != null) {
-						Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-						Palette palette = PaletteTransformation.getPalette(bitmap);
-						callback.onSuccessfulImageLoad(palette);
-					}
-				}
-
-				@Override
-				public void onError() {
-					safelyLoadImage(imageView, imageUrls, callback);
-				}
-			});
-	}
 
 	/**
-	 * Append a suffix to an image URL. Assumes the URL has no suffix (but may have an extension or not.
+	 * Append a suffix to an image URL. Assumes the URL has no suffix (but may have an extension).
 	 */
 	public static String appendImageUrl(String imageUrl, String suffix) {
-		if (TextUtils.isEmpty(imageUrl)) {
-			return "";
-		}
-		if (TextUtils.isEmpty(suffix)) {
-			return imageUrl;
-		}
+		if (TextUtils.isEmpty(imageUrl)) return "";
+		if (TextUtils.isEmpty(suffix)) return imageUrl;
 		int dot = imageUrl.lastIndexOf('.');
 		if (dot == -1) {
 			return imageUrl + suffix;
@@ -150,11 +93,33 @@ public class ImageUtils {
 		void onFailedImageLoad();
 	}
 
-	public static void loadThumbnail(int imageId, ImageView target) {
-		Queue<String> queue = new LinkedList<>();
-		queue.add(ImageUtils.createThumbnailJpgUrl(imageId));
-		queue.add(ImageUtils.createThumbnailPngUrl(imageId));
-		safelyLoadThumbnail(target, queue);
+	public static void loadThumbnail(final int imageId, final ImageView target) {
+		Call<Image> call = Adapter.createGeekdoApi().image(imageId);
+		call.enqueue(new retrofit2.Callback<Image>() {
+			@Override
+			public void onResponse(@NonNull Call<Image> call, @NonNull Response<Image> response) {
+				if (response.code() == 200 && response.body() != null) {
+					Queue<String> queue = new LinkedList<>();
+					queue.add(response.body().images.small.url);
+					addDefaultThumbnailsToQueue(queue, imageId);
+					safelyLoadThumbnail(target, queue);
+				} else {
+					safelyLoadThumbnail(target, addDefaultThumbnailsToQueue(null, imageId));
+				}
+			}
+
+			@Override
+			public void onFailure(@NonNull Call<Image> call, @NonNull Throwable t) {
+				safelyLoadThumbnail(target, addDefaultThumbnailsToQueue(null, imageId));
+			}
+		});
+	}
+
+	private static Queue<String> addDefaultThumbnailsToQueue(Queue<String> queue, int imageId) {
+		if (queue == null) queue = new LinkedList<>();
+		queue.add(IMAGE_URL_PREFIX + imageId + SUFFIX_SMALL + ".jpg");
+		queue.add(IMAGE_URL_PREFIX + imageId + SUFFIX_SMALL + ".png");
+		return queue;
 	}
 
 	public static void loadThumbnail(String path, ImageView target) {
@@ -164,25 +129,81 @@ public class ImageUtils {
 	}
 
 	private static void safelyLoadThumbnail(final ImageView imageView, final Queue<String> imageUrls) {
-		final String imageUrl = imageUrls.poll();
-		if (TextUtils.isEmpty(imageUrl)) {
+		String savedUrl = (String) imageView.getTag(R.id.url);
+		String url = null;
+		if (!TextUtils.isEmpty(savedUrl)) {
+			if (imageUrls.contains(savedUrl)) {
+				url = savedUrl;
+			} else {
+				imageView.setTag(R.id.url, null);
+			}
+		}
+		if (TextUtils.isEmpty(url)) {
+			url = imageUrls.poll();
+		}
+		if (TextUtils.isEmpty(url)) {
 			return;
 		}
+		final String imageUrl = url;
 		Picasso.with(imageView.getContext())
-			.load(HttpUtils.ensureScheme(imageUrl))
-			.placeholder(R.drawable.thumbnail_image_empty)
-			.error(R.drawable.thumbnail_image_empty)
-			.resizeDimen(R.dimen.thumbnail_list_size, R.dimen.thumbnail_list_size)
-			.centerCrop()
-			.into(imageView, new com.squareup.picasso.Callback() {
-				@Override
-				public void onSuccess() {
-				}
+				.load(HttpUtils.ensureScheme(imageUrl))
+				.placeholder(R.drawable.thumbnail_image_empty)
+				.error(R.drawable.thumbnail_image_empty)
+				.resizeDimen(R.dimen.thumbnail_list_size, R.dimen.thumbnail_list_size)
+				.centerCrop()
+				.into(imageView, new com.squareup.picasso.Callback() {
+					@Override
+					public void onSuccess() {
+						imageView.setTag(R.id.url, imageUrl);
+					}
 
-				@Override
-				public void onError() {
-					safelyLoadThumbnail(imageView, imageUrls);
-				}
-			});
+					@Override
+					public void onError() {
+						safelyLoadThumbnail(imageView, imageUrls);
+					}
+				});
+	}
+
+	/**
+	 * Loads an image into the {@link android.widget.ImageView} by attempting each URL in the {@link java.util.Queue}
+	 * until one is successful. Applies fit/center crop and will load a {@link android.support.v7.graphics.Palette}.
+	 */
+	private static void safelyLoadImage(final ImageView imageView, final Queue<String> imageUrls, final Callback callback) {
+		String savedUrl = (String) imageView.getTag(R.id.url);
+		String url = null;
+		if (!TextUtils.isEmpty(savedUrl)) {
+			if (imageUrls.contains(savedUrl)) {
+				url = savedUrl;
+			} else {
+				imageView.setTag(R.id.url, null);
+			}
+		}
+		if (TextUtils.isEmpty(url)) {
+			url = imageUrls.poll();
+		}
+		if (TextUtils.isEmpty(url)) {
+			if (callback != null) callback.onFailedImageLoad();
+			return;
+		}
+		final String imageUrl = url;
+		Picasso.with(imageView.getContext())
+				.load(HttpUtils.ensureScheme(imageUrl))
+				.transform(PaletteTransformation.instance())
+				.into(imageView, new com.squareup.picasso.Callback() {
+					@Override
+					public void onSuccess() {
+						imageView.setTag(R.id.url, imageUrl);
+						if (callback != null) {
+							Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+							Palette palette = PaletteTransformation.getPalette(bitmap);
+							callback.onSuccessfulImageLoad(palette);
+						}
+					}
+
+					@Override
+					public void onError() {
+						safelyLoadImage(imageView, imageUrls, callback);
+					}
+				});
 	}
 }
