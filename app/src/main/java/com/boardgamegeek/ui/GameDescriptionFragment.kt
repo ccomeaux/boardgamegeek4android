afc198d90107f04f3e2fc6bf72146cb5d661cff7
Chris Comeaux <chris.comeaux@gmail.com>
2018-05-09 11:08:11 -0500

    Make GameDescriptionFragment more idomatic

--- app/src/main/java/com/boardgamegeek/ui/GameDescriptionFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/GameDescriptionFragment.kt
@@ -1,11 +1,9 @@
 package com.boardgamegeek.ui
 
-
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,74 +14,60 @@ import com.boardgamegeek.ui.model.Status
 import com.boardgamegeek.ui.viewmodel.GameViewModel
 import kotlinx.android.synthetic.main.fragment_game_description.*
 import kotlinx.android.synthetic.main.include_game_footer.*
+import org.jetbrains.anko.support.v4.act
 
-class GameDescriptionFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
+class GameDescriptionFragment : Fragment() {
     private var gameId: Int = 0
-    private lateinit var viewModel: GameViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            gameId = arguments!!.getInt(ARG_GAME_ID, BggContract.INVALID_ID)
-        }
-        if (gameId == BggContract.INVALID_ID) throw IllegalArgumentException("Invalid game ID")
-        viewModel = ViewModelProviders.of(activity!!).get(GameViewModel::class.java)
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProviders.of(act).get(GameViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_game_description, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-        swipe_refresh.setOnRefreshListener(this)
-        swipe_refresh.setBggColors()
+        gameId = arguments?.getInt(ARG_GAME_ID, BggContract.INVALID_ID) ?: BggContract.INVALID_ID
+        if (gameId == BggContract.INVALID_ID) throw IllegalArgumentException("Invalid game ID")
+
+        swipeRefresh.setOnRefreshListener { viewModel.refresh() }
+        swipeRefresh.setBggColors()
 
         game_info_id.text = gameId.toString()
         game_info_last_updated.timestamp = 0L
 
         viewModel.getGame(gameId).observe(this, Observer { game ->
-            updateRefreshStatus(game?.status == Status.REFRESHING)
+            swipeRefresh?.post { swipeRefresh?.isRefreshing = game?.status == Status.REFRESHING }
             when {
-                game == null -> showEmpty()
+                game == null -> showError(getString(R.string.empty_game))
                 game.status == Status.ERROR && game.data == null -> showError(game.message)
-                game.data == null -> showEmpty()
+                game.data == null -> showError(getString(R.string.empty_game))
                 else -> showData(game.data)
             }
         })
     }
 
-    override fun onRefresh() {
-        viewModel.refresh()
-    }
-
-    private fun showEmpty() {
-        showError(getString(R.string.empty_game))
-    }
-
     private fun showError(message: String?) {
         if (message?.isNotBlank() == true) {
-            empty.text = message
-            game_description.fadeOut()
-            empty.fadeIn()
+            emptyMessage.text = message
+            gameDescription.fadeOut()
+            emptyMessage.fadeIn()
             progress.hide()
         }
     }
 
     private fun showData(game: Game) {
-        empty.fadeOut()
+        emptyMessage.fadeOut()
         progress.hide()
 
-        game_description.setTextMaybeHtml(game.description)
-        game_description.fadeIn()
+        gameDescription.setTextMaybeHtml(game.description)
+        gameDescription.fadeIn()
 
         game_info_id.text = game.id.toString()
         game_info_last_updated.timestamp = game.updated
-    }
-
-    private fun updateRefreshStatus(refreshing: Boolean) {
-        swipe_refresh?.post { swipe_refresh?.isRefreshing = refreshing }
     }
 
     companion object {
