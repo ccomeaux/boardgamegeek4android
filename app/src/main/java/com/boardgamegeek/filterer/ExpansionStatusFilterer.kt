e7a8f60abbdce6d08acf283c7063c3a1ed85cd77
Chris Comeaux <chris.comeaux@gmail.com>
2018-07-03 22:22:29 -0500

    Improve expansion filter

--- app/src/main/java/com/boardgamegeek/filterer/ExpansionStatusFilterer.kt
+++ app/src/main/java/com/boardgamegeek/filterer/ExpansionStatusFilterer.kt
@@ -3,31 +3,32 @@ package com.boardgamegeek.filterer
 import android.content.Context
 import com.boardgamegeek.R
 import com.boardgamegeek.provider.BggContract.Games
-import com.boardgamegeek.util.StringUtils
 
 class ExpansionStatusFilterer(context: Context) : CollectionFilterer(context) {
-    var selectedSubtype = 0
+    var selectedSubtype = ALL
 
     override val typeResourceId = R.string.collection_filter_type_subtype
 
     override fun inflate(data: String) {
-        selectedSubtype = StringUtils.parseInt(data)
+        selectedSubtype = data.toIntOrNull() ?: ALL
     }
 
     override fun deflate() = selectedSubtype.toString()
 
-    override fun toShortDescription() = getSelectedFromStringArray(R.array.expansion_status_filter)
+    override fun toShortDescription() = getFromArray(R.array.expansion_status_filter)
 
     override fun getSelection(): String {
-        val value = getSelectedFromStringArray(R.array.expansion_status_filter_values)
-        return if (value.isNotEmpty()) Games.SUBTYPE + "=?" else ""
+        val value = getFromArray(R.array.expansion_status_filter_values)
+        return if (value.isNotEmpty()) "${Games.SUBTYPE}=?" else ""
     }
 
-    override fun getSelectionArgs() =
-            arrayOf(getSelectedFromStringArray(R.array.expansion_status_filter_values))
+    override fun getSelectionArgs() = arrayOf(getFromArray(R.array.expansion_status_filter_values))
 
-    private fun getSelectedFromStringArray(resId: Int): String {
-        val values = context.resources.getStringArray(resId)
-        return values.getOrNull(selectedSubtype) ?: ""
+    private fun getFromArray(resId: Int): String {
+        return context.resources.getStringArray(resId).getOrNull(selectedSubtype) ?: ""
+    }
+
+    companion object {
+        const val ALL = 0
     }
 }
