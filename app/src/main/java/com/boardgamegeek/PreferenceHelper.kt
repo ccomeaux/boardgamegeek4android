b3e75e9b8c7c669425ec52d6af0436a88a0616a1
Chris Comeaux <chris.comeaux@gmail.com>
2018-07-18 14:14:10 -0500

    Change PreferenceHelper to remove non-idiomatic parentheses.

--- app/src/main/java/com/boardgamegeek/PreferenceHelper.kt
+++ app/src/main/java/com/boardgamegeek/PreferenceHelper.kt
@@ -23,11 +23,11 @@ object PreferenceHelper {
      */
     operator fun SharedPreferences.set(key: String, value: Any?) {
         when (value) {
-            is String? -> edit({ it.putString(key, value) })
-            is Int -> edit({ it.putInt(key, value) })
-            is Boolean -> edit({ it.putBoolean(key, value) })
-            is Float -> edit({ it.putFloat(key, value) })
-            is Long -> edit({ it.putLong(key, value) })
+            is String? -> edit { it.putString(key, value) }
+            is Int -> edit { it.putInt(key, value) }
+            is Boolean -> edit { it.putBoolean(key, value) }
+            is Float -> edit { it.putFloat(key, value) }
+            is Long -> edit { it.putLong(key, value) }
             else -> throw UnsupportedOperationException("Not yet implemented")
         }
     }
