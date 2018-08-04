2e0fe9610bc464ee0033c6f98e7f04bb46c6e481
Chris Comeaux <chris.comeaux@gmail.com>
2018-08-04 06:40:29 -0500

    RC4 - Log the unparsed content and let the app throw an exception

--- app/src/main/java/com/boardgamegeek/model/PlayPostResponse.java
+++ app/src/main/java/com/boardgamegeek/model/PlayPostResponse.java
@@ -3,10 +3,10 @@ package com.boardgamegeek.model;
 import android.support.annotation.NonNull;
 import android.text.Html;
 import android.text.TextUtils;
+import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 
@@ -14,7 +14,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import timber.log.Timber;
 
 public abstract class PlayPostResponse {
 	protected final Gson gson = new Gson();
@@ -28,27 +27,17 @@ public abstract class PlayPostResponse {
 			if (response.isSuccessful()) {
 				final ResponseBody body = response.body();
 				final String content = body == null ? "" : body.string().trim();
+				Crashlytics.log(Log.WARN, "PlaysUpload", content);
 				if (content.startsWith(ERROR_DIV)) {
 					//noinspection deprecation
 					error = Html.fromHtml(content).toString().trim();
 				} else {
-					try {
-						saveContent(content);
-					} catch (JsonSyntaxException e) {
-						Timber.w("Couldn't parse JSON - %s", content);
-						throw new JsonSyntaxException(content, e);
-					} catch (JsonParseException e) {
-						Timber.w("Couldn't parse JSON - %s", content);
-						throw new JsonParseException(content, e);
-					} catch (IllegalStateException e) {
-						Timber.w("Couldn't parse JSON - %s", content);
-						throw new IllegalStateException(content, e);
-					}
+					saveContent(content);
 				}
 			} else {
 				error = "Unsuccessful post: " + response.code();
 			}
-		} catch (IOException | IllegalStateException e) {
+		} catch (IOException e) {
 			exception = e;
 		}
 	}
