af89fb611caabfc9efff941e5aa7c6d17a1da003
Chris Comeaux <chris.comeaux@gmail.com>
2018-08-29 21:38:28 -0500

    Correctly collect the acquired info from the dialog fragment

--- app/src/main/java/com/boardgamegeek/ui/model/PrivateInfo.kt
+++ app/src/main/java/com/boardgamegeek/ui/model/PrivateInfo.kt
@@ -6,7 +6,7 @@ data class PrivateInfo(
         val currentValueCurrency: String? = null,
         val currentValue: Double = 0.toDouble(),
         val quantity: Int = 1,
-        val acquiredFrom: String? = null,
         val acquisitionDate: String? = null,
+        val acquiredFrom: String? = null,
         val inventoryLocation: String? = null
 )
