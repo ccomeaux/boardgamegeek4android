d9feb63444ac3291ae1238a4e1f1400a773d86e8
Chris Comeaux <chris.comeaux@gmail.com>
2018-11-06 22:32:57 -0600

    Remove ScrimUtils

--- app/src/main/java/com/boardgamegeek/extensions/View.kt
+++ app/src/main/java/com/boardgamegeek/extensions/View.kt
@@ -1,6 +1,14 @@
 package com.boardgamegeek.extensions
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.PaintDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import android.os.Build
 import android.util.TypedValue
 import android.view.Gravity
@@ -11,10 +19,14 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.alpha
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import androidx.core.view.ViewCompat
 import com.boardgamegeek.R
 import com.boardgamegeek.util.ColorUtils
-import com.boardgamegeek.util.ScrimUtils
+import kotlin.math.pow
 
 fun View.fadeIn(animate: Boolean = true) {
     if (visibility != VISIBLE) {
@@ -54,8 +66,42 @@ fun View.setColorViewValue(color: Int) {
 
 fun View.applyDarkScrim() {
     val color = ContextCompat.getColor(context, R.color.black_overlay)
-    val drawable = ScrimUtils.makeCubicGradientScrimDrawable(color, 3, Gravity.BOTTOM)
+    val drawable = makeCubicGradientScrimDrawable(color)
     ViewCompat.setBackground(this, drawable)
+}
+
+@SuppressLint("RtlHardcoded")
+private fun makeCubicGradientScrimDrawable(@ColorInt baseColor: Int, numberOfStops: Int = 3, gravity: Int = Gravity.BOTTOM): Drawable {
+    val numStops = numberOfStops.coerceAtLeast(2)
+    val paintDrawable = PaintDrawable().apply {
+        shape = RectShape()
+    }
+    val stopColors = IntArray(numStops)
+
+    for (i in 0 until numStops) {
+        val opacity = (i * 1f / (numStops - 1)).toDouble().pow(3).toFloat().coerceIn(0f, 1f)
+        stopColors[i] = Color.argb((baseColor.alpha * opacity).toInt(), baseColor.red, baseColor.green, baseColor.blue)
+    }
+
+    val x0 = if (gravity and Gravity.HORIZONTAL_GRAVITY_MASK == Gravity.LEFT) 1f else 0f
+    val x1 = if (gravity and Gravity.HORIZONTAL_GRAVITY_MASK == Gravity.RIGHT) 1f else 0f
+    val y0 = if (gravity and Gravity.VERTICAL_GRAVITY_MASK == Gravity.TOP) 1f else 0f
+    val y1 = if (gravity and Gravity.VERTICAL_GRAVITY_MASK == Gravity.BOTTOM) 1f else 0f
+
+    paintDrawable.shaderFactory = object : ShapeDrawable.ShaderFactory() {
+        override fun resize(width: Int, height: Int): Shader {
+            return LinearGradient(
+                    width * x0,
+                    height * y0,
+                    width * x1,
+                    height * y1,
+                    stopColors,
+                    null,
+                    Shader.TileMode.CLAMP)
+        }
+    }
+
+    return paintDrawable
 }
 
 /**
