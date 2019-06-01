9297330b2d6e6607cbcdbee58c073b2ebb353a92
Chris Comeaux <chris.comeaux@gmail.com>
2019-05-31 23:43:38 -0500

    Convert top game classes to Kotlin

---
+++ app/src/main/java/com/boardgamegeek/entities/TopGameEntity.kt
@@ -0,0 +1,9 @@
+package com.boardgamegeek.entities
+
+data class TopGameEntity(
+        val id: Int,
+        val name: String,
+        val rank: Int,
+        val yearPublished: Int,
+        val thumbnailUrl: String
+)
