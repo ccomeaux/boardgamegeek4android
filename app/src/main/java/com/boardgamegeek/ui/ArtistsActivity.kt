625fb14db425e33b3cd712c20776f1f0f4de3123
Chris Comeaux <chris.comeaux@gmail.com>
2019-07-05 20:38:45 -0500

    Only allow sorting by Whitmore score if rating is synced

--- app/src/main/java/com/boardgamegeek/ui/ArtistsActivity.kt
+++ app/src/main/java/com/boardgamegeek/ui/ArtistsActivity.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.boardgamegeek.R
+import com.boardgamegeek.extensions.COLLECTION_STATUS_RATED
+import com.boardgamegeek.extensions.isStatusSetToSync
 import com.boardgamegeek.extensions.setActionBarCount
 import com.boardgamegeek.ui.viewmodel.ArtistsViewModel
 
@@ -45,6 +47,9 @@ class ArtistsActivity : SimpleSinglePaneActivity() {
             isChecked = true
             menu.setActionBarCount(R.id.menu_list_count, numberOfArtists, getString(R.string.by_prefix, title))
         }
+        val showWhitmoreScore = isStatusSetToSync(COLLECTION_STATUS_RATED)
+        val whitmoreScoreItem = menu.findItem(R.id.menu_sort_whitmore_score)
+        whitmoreScoreItem.isVisible = showWhitmoreScore || whitmoreScoreItem.isChecked
         return true
     }
 
