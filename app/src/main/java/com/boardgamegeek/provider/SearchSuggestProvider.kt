f7074f112065a387d14ece68dcab6782c4b75048
Chris Comeaux <chris.comeaux@gmail.com>
2020-05-03 07:30:07 -0500

    Convert file providers to Kotlin Search thumbnail again in search results (only works if you created a shortcut to get the image stored in the right place)

--- app/src/main/java/com/boardgamegeek/provider/SearchSuggestProvider.kt
+++ app/src/main/java/com/boardgamegeek/provider/SearchSuggestProvider.kt
@@ -11,7 +11,9 @@ import com.boardgamegeek.provider.BggContract.CollectionColumns.COLLECTION_NAME
 import com.boardgamegeek.provider.BggContract.CollectionColumns.COLLECTION_YEAR_PUBLISHED
 import com.boardgamegeek.provider.BggContract.GamesColumns.GAME_ID
 import com.boardgamegeek.provider.BggContract.GamesColumns.LAST_VIEWED
+import com.boardgamegeek.provider.BggContract.PATH_THUMBNAILS
 import com.boardgamegeek.provider.BggDatabase.Tables
+import java.util.*
 
 /**
  * Called from the search widget to populate the drop down list.
@@ -25,7 +27,7 @@ open class SearchSuggestProvider : BaseProvider() {
     override fun getDefaultSortOrder() = BggContract.Collection.DEFAULT_SORT
 
     override fun query(resolver: ContentResolver, db: SQLiteDatabase, uri: Uri?, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
-        val searchTerm = uri?.lastPathSegment.orEmpty()
+        val searchTerm = uri?.lastPathSegment?.toLowerCase(Locale.getDefault()).orEmpty()
         val limit = uri?.getQueryParameter(SearchManager.SUGGEST_PARAMETER_LIMIT)
         val qb = SQLiteQueryBuilder().apply {
             tables = Tables.COLLECTION_JOIN_GAMES
@@ -44,7 +46,7 @@ open class SearchSuggestProvider : BaseProvider() {
                 BaseColumns._ID to "${Tables.GAMES}.${BaseColumns._ID}",
                 SearchManager.SUGGEST_COLUMN_TEXT_1 to "$COLLECTION_NAME AS ${SearchManager.SUGGEST_COLUMN_TEXT_1}",
                 SearchManager.SUGGEST_COLUMN_TEXT_2 to "IFNULL(CASE WHEN $COLLECTION_YEAR_PUBLISHED=0 THEN NULL ELSE $COLLECTION_YEAR_PUBLISHED END, '?') AS ${SearchManager.SUGGEST_COLUMN_TEXT_2}",
-                //SearchManager.SUGGEST_COLUMN_ICON_2 to "'${Games.CONTENT_URI}/' || ${Tables.COLLECTION}.$GAME_ID || '/$PATH_THUMBNAILS' AS ${SearchManager.SUGGEST_COLUMN_ICON_2}",
+                SearchManager.SUGGEST_COLUMN_ICON_2 to "'${BggContract.Games.CONTENT_URI}/' || ${Tables.COLLECTION}.$GAME_ID || '/$PATH_THUMBNAILS' AS ${SearchManager.SUGGEST_COLUMN_ICON_2}",
                 SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID to "${Tables.GAMES}.$GAME_ID AS ${SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID}",
                 SearchManager.SUGGEST_COLUMN_INTENT_EXTRA_DATA to "$COLLECTION_NAME AS ${SearchManager.SUGGEST_COLUMN_INTENT_EXTRA_DATA}",
                 SearchManager.SUGGEST_COLUMN_LAST_ACCESS_HINT to "$LAST_VIEWED AS ${SearchManager.SUGGEST_COLUMN_LAST_ACCESS_HINT}"
--- app/src/main/java/com/boardgamegeek/provider/ThumbnailsProvider.kt
+++ app/src/main/java/com/boardgamegeek/provider/ThumbnailsProvider.kt
@@ -3,19 +3,19 @@ package com.boardgamegeek.provider
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
-import com.boardgamegeek.provider.BggContract.PATH_AVATARS
+import com.boardgamegeek.provider.BggContract.PATH_THUMBNAILS
 import com.boardgamegeek.util.FileUtils
 import timber.log.Timber
 import java.io.IOException
 
-class AvatarsProvider : BaseProvider() {
-    override fun getPath() = PATH_AVATARS
+class ThumbnailsProvider : BaseProvider() {
+    override fun getPath() = PATH_THUMBNAILS
 
     override fun delete(context: Context, db: SQLiteDatabase, uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
         return try {
-            FileUtils.deleteContents(FileUtils.generateContentPath(context, PATH_AVATARS))
+            FileUtils.deleteContents(FileUtils.generateContentPath(context, PATH_THUMBNAILS))
         } catch (e: IOException) {
-            Timber.e(e, "Couldn't delete avatars")
+            Timber.e(e, "Couldn't delete thumbnails")
             0
         }
     }
