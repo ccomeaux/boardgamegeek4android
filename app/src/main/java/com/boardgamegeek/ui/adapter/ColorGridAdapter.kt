f37b794db23844c81ef9610946de7cbd7ae3c95c
Chris Comeaux <chris.comeaux@gmail.com>
2020-09-29 20:22:11 -0500

    Convert ColorUtils to kotlin

--- app/src/main/java/com/boardgamegeek/ui/adapter/ColorGridAdapter.kt
+++ app/src/main/java/com/boardgamegeek/ui/adapter/ColorGridAdapter.kt
@@ -2,7 +2,6 @@ package com.boardgamegeek.ui.adapter
 
 import android.content.Context
 import android.graphics.Color
-import android.util.Pair
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/boardgamegeek/ui/dialog/ColorPickerDialogFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/dialog/ColorPickerDialogFragment.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Pair
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
