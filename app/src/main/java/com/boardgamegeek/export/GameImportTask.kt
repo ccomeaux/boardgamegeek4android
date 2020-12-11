8283ddf192065a933dc2bb4fe4bf8d320f8b740f
Chris Comeaux <chris.comeaux@gmail.com>
2020-12-11 06:50:23 -0600

    Refactor import tasks to Kotlin

---
+++ app/src/main/java/com/boardgamegeek/export/GameImportTask.kt
@@ -0,0 +1,33 @@
+package com.boardgamegeek.export
+
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.contentValuesOf
+import com.boardgamegeek.export.model.Game
+import com.boardgamegeek.extensions.rowExists
+import com.boardgamegeek.provider.BggContract.GameColors
+import com.boardgamegeek.provider.BggContract.Games
+import com.google.gson.Gson
+import com.google.gson.stream.JsonReader
+
+class GameImportTask(context: Context, uri: Uri) : JsonImportTask<Game>(context, Constants.TYPE_GAMES, uri) {
+    override fun parseItem(gson: Gson, reader: JsonReader): Game {
+        return gson.fromJson(reader, Game::class.java)
+    }
+
+    override fun importRecord(item: Game, version: Int) {
+        if (context.contentResolver.rowExists(Games.buildGameUri(item.gameId))) {
+            val gameColorsUri = Games.buildColorsUri(item.gameId)
+            context.contentResolver.delete(gameColorsUri, null, null)
+            val values = arrayListOf<ContentValues>()
+            item.colors.filter { it.color.isNotBlank() }.forEach { color ->
+                values.add(contentValuesOf(GameColors.COLOR to color.color))
+            }
+            if (values.size > 0) {
+                val array = arrayOf<ContentValues>()
+                context.contentResolver.bulkInsert(gameColorsUri, values.toArray(array))
+            }
+        }
+    }
+}
