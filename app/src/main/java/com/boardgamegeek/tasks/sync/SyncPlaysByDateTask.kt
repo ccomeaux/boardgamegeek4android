1f6392897a4bb40c03789108fa8bd57f5e217ee1
Chris Comeaux <chris.comeaux@gmail.com>
2021-02-12 21:57:24 -0600

    Convert sync tasks to Kotlin

---
+++ app/src/main/java/com/boardgamegeek/tasks/sync/SyncPlaysByDateTask.kt
@@ -0,0 +1,48 @@
+package com.boardgamegeek.tasks.sync
+
+import androidx.annotation.StringRes
+import com.boardgamegeek.BggApplication
+import com.boardgamegeek.R
+import com.boardgamegeek.auth.AccountUtils
+import com.boardgamegeek.extensions.executeAsyncTask
+import com.boardgamegeek.io.model.PlaysResponse
+import com.boardgamegeek.mappers.PlayMapper
+import com.boardgamegeek.model.persister.PlayPersister
+import com.boardgamegeek.tasks.CalculatePlayStatsTask
+import com.boardgamegeek.util.DateTimeUtils
+import retrofit2.Call
+import timber.log.Timber
+
+class SyncPlaysByDateTask(private val application: BggApplication, year: Int, month: Int, day: Int) : SyncTask<PlaysResponse>(application.applicationContext) {
+    private val date: String = DateTimeUtils.formatDateForApi(year, month, day)
+    private val username = AccountUtils.getUsername(context)
+    private val persister = PlayPersister(context)
+    private val mapper = PlayMapper()
+
+    @get:StringRes
+    override val typeDescriptionResId: Int
+        get() = R.string.title_plays
+
+    override fun createCall(): Call<PlaysResponse>? {
+        return bggService?.playsByDate(username, date, date, currentPage)
+    }
+
+    override val isRequestParamsValid: Boolean
+        get() = date.isNotBlank() && !username.isNullOrBlank()
+
+    override fun persistResponse(body: PlaysResponse?) {
+        body?.plays?.let {
+            val plays = mapper.map(it)
+            persister.save(plays, startTime)
+        }
+        Timber.i("Synced plays for %s (page %,d)", date, currentPage)
+    }
+
+    override fun hasMorePages(body: PlaysResponse?): Boolean {
+        return body?.hasMorePages() == true
+    }
+
+    override fun finishSync() {
+        CalculatePlayStatsTask(application).executeAsyncTask()
+    }
+}
