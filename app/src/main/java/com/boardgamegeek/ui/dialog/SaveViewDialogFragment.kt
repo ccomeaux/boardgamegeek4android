3e2e97c3434ba7fdbb8b3a22a5781333cbee8971
Chris Comeaux <chris.comeaux@gmail.com>
2021-03-28 21:46:54 -0500

    Use import doAfterTextChanged instead of implementing entire TextWatcher

--- app/src/main/java/com/boardgamegeek/ui/dialog/SaveViewDialogFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/dialog/SaveViewDialogFragment.kt
@@ -3,14 +3,13 @@ package com.boardgamegeek.ui.dialog
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.boardgamegeek.R
@@ -124,15 +123,9 @@ class SaveViewDialogFragment : DialogFragment() {
 
         private fun enableSaveButton(dialog: AlertDialog, nameView: EditText) {
             dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = !nameView.text.isNullOrBlank()
-            nameView.addTextChangedListener(object : TextWatcher {
-                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-
-                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-                override fun afterTextChanged(s: Editable) {
-                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = !nameView.text.isNullOrBlank()
-                }
-            })
+            nameView.doAfterTextChanged {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = !nameView.text.isNullOrBlank()
+            }
         }
     }
 }
