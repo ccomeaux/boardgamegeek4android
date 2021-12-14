171459b5c107a5933d9575c65b64aaa34a535361
Chris Comeaux <chris.comeaux@gmail.com>
2021-12-13 21:14:03 -0600

    GameRanksFragment - use viewbinding, improve layout, refactor

--- app/src/main/java/com/boardgamegeek/ui/dialog/GameRanksFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/dialog/GameRanksFragment.kt
@@ -1,73 +1,87 @@
 package com.boardgamegeek.ui.dialog
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.boardgamegeek.R
+import com.boardgamegeek.databinding.DialogGameRanksBinding
 import com.boardgamegeek.extensions.*
 import com.boardgamegeek.ui.viewmodel.GameViewModel
 import com.boardgamegeek.ui.widget.GameRankRow
-import kotlinx.android.synthetic.main.dialog_game_ranks.*
 import timber.log.Timber
 
-class GameRanksFragment : DialogFragment(R.layout.dialog_game_ranks) {
+class GameRanksFragment : DialogFragment() {
+    private var _binding: DialogGameRanksBinding? = null
+    private val binding get() = _binding!!
     private val viewModel by activityViewModels<GameViewModel>()
+
+    @Suppress("RedundantNullableReturnType")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DialogGameRanksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dialog?.setTitle(R.string.title_ranks_ratings)
 
-        viewModel.game.observe(this, {
+        viewModel.game.observe(viewLifecycleOwner) {
             val voteCount = it?.data?.numberOfRatings ?: 0
             val standardDeviation = it?.data?.standardDeviation ?: 0.0
-            votesView?.text = context?.getQuantityText(R.plurals.votes_suffix, voteCount, voteCount)
-            if (voteCount == 0) {
-                standardDeviationView?.visibility = View.GONE
-            } else {
-                standardDeviationView?.text = context?.getText(R.string.standard_deviation_prefix, standardDeviation) ?: ""
-                standardDeviationView?.visibility = View.VISIBLE
-            }
-        })
+            binding.votesView.text = requireContext().getQuantityText(R.plurals.votes_suffix, voteCount, voteCount)
+            binding.standardDeviationView.text = requireContext().getText(R.string.standard_deviation_prefix, standardDeviation)
+            binding.standardDeviationView.isVisible = voteCount > 0
+        }
 
-        viewModel.ranks.observe(this, { gameRankEntities ->
-            subtypesView?.removeAllViews()
-            familiesView?.removeAllViews()
+        viewModel.ranks.observe(viewLifecycleOwner) { gameRankEntities ->
+            binding.unRankedView.isVisible = false
+            binding.subtypesView.removeAllViews()
+            binding.subtypesView.isVisible = false
+            binding.familiesView.removeAllViews()
+            binding.familiesView.isVisible = false
 
             var hasRankedSubtype = false
             var unRankedSubtype = getText(R.string.game)
 
-            if (gameRankEntities?.isNotEmpty() == true) {
-                gameRankEntities.forEach { rank ->
-                    if (rank.value.isRankValid()) {
-                        val row = GameRankRow(requireContext(), rank.isFamilyType)
-                        row.setRank(rank.value)
-                        row.setName(rank.name.asRankDescription(requireContext(), rank.type))
-                        row.setRatingView(rank.bayesAverage)
-                        when {
-                            rank.isSubType -> {
-                                subtypesView?.addView(row)
-                                subtypesView?.visibility = View.VISIBLE
-                                unRankedView?.visibility = View.GONE
-                                hasRankedSubtype = true
-                            }
-                            rank.isFamilyType -> {
-                                familiesView?.addView(row)
-                                familiesView?.visibility = View.VISIBLE
-                            }
-                            else -> Timber.i("Invalid rank type: ${rank.type}")
-                        }
-                    } else if (rank.isSubType) {
-                        unRankedSubtype = rank.name.asRankDescription(requireContext(), rank.type)
+            gameRankEntities?.forEach { rank ->
+                if (rank.value.isRankValid()) {
+                    val row = GameRankRow(requireContext(), rank.isFamilyType).apply {
+                        setRank(rank.value)
+                        setName(rank.name.asRankDescription(requireContext(), rank.type))
+                        setRatingView(rank.bayesAverage)
                     }
+                    when {
+                        rank.isSubType -> {
+                            binding.subtypesView.addView(row)
+                            binding.subtypesView.isVisible = true
+                            binding.unRankedView.isVisible = false
+                            hasRankedSubtype = true
+                        }
+                        rank.isFamilyType -> {
+                            binding.familiesView.addView(row)
+                            binding.familiesView.isVisible = true
+                        }
+                        else -> Timber.i("Invalid rank type: ${rank.type}")
+                    }
+                } else if (rank.isSubType) {
+                    unRankedSubtype = rank.name.asRankDescription(requireContext(), rank.type)
                 }
             }
             if (!hasRankedSubtype && unRankedSubtype.isNotEmpty()) {
-                unRankedView?.text = context?.getText(R.string.unranked_prefix, unRankedSubtype) ?: ""
-                unRankedView?.visibility = View.VISIBLE
+                binding.unRankedView.text = requireContext().getText(R.string.unranked_prefix, unRankedSubtype)
+                binding.unRankedView.isVisible = true
             }
-        })
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
