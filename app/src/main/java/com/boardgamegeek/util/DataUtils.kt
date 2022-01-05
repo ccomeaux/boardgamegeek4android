e05f66dcebde63e64e0d738d94c997a8ca227856
Chris Comeaux <chris.comeaux@gmail.com>
2022-01-04 22:05:56 -0600

    Convert DataUtils to Kotlin

---
+++ app/src/main/java/com/boardgamegeek/util/DataUtils.kt
@@ -0,0 +1,19 @@
+package com.boardgamegeek.util
+
+object DataUtils {
+    fun generatePollResultsKey(level: Int, value: String): String {
+        return if (level <= 0) {
+            generatePollResultsKey("", value)
+        } else {
+            generatePollResultsKey(level.toString(), value)
+        }
+    }
+
+    fun generatePollResultsKey(level: String, value: String): String {
+        return if (level.isEmpty()) {
+            value.substringBefore(" ")
+        } else {
+            level
+        }
+    }
+}
