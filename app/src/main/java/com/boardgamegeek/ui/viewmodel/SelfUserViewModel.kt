3891f3db7eceaf5ac867158342efa6a2b15d43b3
Chris Comeaux <chris.comeaux@gmail.com>
2024-08-02 21:57:50 -0500

    Correct the logic for refreshing the self user

--- app/src/main/java/com/boardgamegeek/ui/viewmodel/SelfUserViewModel.kt
+++ app/src/main/java/com/boardgamegeek/ui/viewmodel/SelfUserViewModel.kt
@@ -2,13 +2,14 @@ package com.boardgamegeek.ui.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.*
-import com.boardgamegeek.model.User
 import com.boardgamegeek.extensions.AccountPreferences
 import com.boardgamegeek.extensions.isOlderThan
 import com.boardgamegeek.livedata.LiveSharedPreference
+import com.boardgamegeek.model.User
 import com.boardgamegeek.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.days
@@ -23,14 +24,14 @@ class SelfUserViewModel @Inject constructor(
     val user: LiveData<User?> = username.switchMap { username ->
         liveData {
             if (!username.isNullOrBlank()) {
-                emitSource(userRepository.loadUserFlow(username).distinctUntilChanged().asLiveData().also {
-                    if (it.value == null || it.value?.updatedTimestamp.isOlderThan(1.days)) {
+                emitSource(userRepository.loadUserFlow(username).distinctUntilChanged().onEach {
+                    if (it == null || it.updatedTimestamp.isOlderThan(1.days)) {
                         val errorMessage = userRepository.refresh(username, true)
                         if (!errorMessage.isNullOrBlank()) {
                             Timber.w(errorMessage)
                         }
                     }
-                })
+                }.asLiveData())
             } else {
                 emit(null)
                 userRepository.updateSelf(null)
