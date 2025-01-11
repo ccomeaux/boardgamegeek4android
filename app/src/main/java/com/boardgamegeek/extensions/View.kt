1deaca578770df973daaee7bdf3965e547ab0649
Chris Comeaux <chris.comeaux@gmail.com>
2025-01-11 09:35:40 -0600

    Remove deprecated removeLast calls

--- app/src/main/java/com/boardgamegeek/extensions/View.kt
+++ app/src/main/java/com/boardgamegeek/extensions/View.kt
@@ -254,7 +254,7 @@ private class ViewChildrenRecursiveSequence(private val view: View) : Sequence<V
 
     private class RecursiveViewIterator(view: ViewGroup) : Iterator<View> {
         private val sequences = arrayListOf(view.children)
-        private var current = sequences.removeLast().iterator()
+        private var current = sequences.removeAt(sequences.lastIndex).iterator()
 
         override fun next(): View {
             if (!hasNext()) throw NoSuchElementException()
@@ -267,7 +267,7 @@ private class ViewChildrenRecursiveSequence(private val view: View) : Sequence<V
 
         override fun hasNext(): Boolean {
             if (!current.hasNext() && sequences.isNotEmpty()) {
-                current = sequences.removeLast().iterator()
+                current = sequences.removeAt(sequences.lastIndex).iterator()
             }
             return current.hasNext()
         }
