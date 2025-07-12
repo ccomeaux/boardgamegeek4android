b9c306ff42fd18195a4a200b03a347adbc1cf8fd
Chris Comeaux <chris.comeaux@gmail.com>
2025-07-12 18:24:29 -0500

    Remove row_mechanic.xml and use Compose for mechanic list items.
    The `MechanicsFragment` now uses a Composable function, `MechanicListItem`, to render each item in the list. This replaces the old XML layout file `row_mechanic.xml`. Previews and a preview parameter provider have been added for `MechanicListItem`.

--- app/src/main/java/com/boardgamegeek/ui/MechanicsFragment.kt
+++ app/src/main/java/com/boardgamegeek/ui/MechanicsFragment.kt
@@ -4,6 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -12,9 +25,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.boardgamegeek.R
 import com.boardgamegeek.databinding.FragmentMechanicsBinding
-import com.boardgamegeek.databinding.RowMechanicBinding
 import com.boardgamegeek.model.Mechanic
-import com.boardgamegeek.extensions.inflate
+import com.boardgamegeek.ui.compose.ListItemPrimaryText
+import com.boardgamegeek.ui.compose.ListItemSecondaryText
+import com.boardgamegeek.ui.compose.ListItemTokens
+import com.boardgamegeek.ui.theme.BggAppTheme
 import com.boardgamegeek.ui.viewmodel.MechanicsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -62,7 +77,7 @@ class MechanicsFragment : Fragment() {
         override fun getItemId(position: Int) = getItem(position).id.toLong()
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MechanicViewHolder {
-            return MechanicViewHolder(parent.inflate(R.layout.row_mechanic))
+            return MechanicViewHolder(ComposeView(parent.context))
         }
 
         override fun onBindViewHolder(holder: MechanicViewHolder, position: Int) {
@@ -75,16 +90,69 @@ class MechanicsFragment : Fragment() {
             override fun areContentsTheSame(oldItem: Mechanic, newItem: Mechanic) = oldItem == newItem
         }
 
-        inner class MechanicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val binding = RowMechanicBinding.bind(itemView)
-
+        inner class MechanicViewHolder(private val composeView: ComposeView) : RecyclerView.ViewHolder(composeView) {
             fun bind(mechanic: Mechanic) {
-                binding.nameView.text = mechanic.name
-                binding.countView.text = itemView.context.resources.getQuantityString(R.plurals.games_suffix, mechanic.itemCount, mechanic.itemCount)
-                itemView.setOnClickListener {
-                    MechanicActivity.start(itemView.context, mechanic.id, mechanic.name)
+                composeView.setContent {
+                    MechanicListItem(
+                        mechanic,
+                        onClick = {
+                            MechanicActivity.start(itemView.context, mechanic.id, mechanic.name)
+                        },
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+private fun MechanicListItem(
+    mechanic: Mechanic,
+    modifier: Modifier = Modifier,
+    onClick: (Mechanic) -> Unit = {},
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+            .background(MaterialTheme.colorScheme.surface)
+            .clickable(onClick = { onClick(mechanic) })
+            .padding(ListItemTokens.paddingValues)
+            .then(modifier)
+    ) {
+        ListItemPrimaryText(mechanic.name)
+        ListItemSecondaryText(pluralStringResource(R.plurals.games_suffix, mechanic.itemCount, mechanic.itemCount))
+    }
+}
+
+@Preview
+@Composable
+private fun MechanicListItemPreview(
+    @PreviewParameter(MechanicPreviewParameterProvider::class) mechanic: Mechanic
+) {
+    BggAppTheme {
+        MechanicListItem(mechanic)
+    }
+}
+
+private class MechanicPreviewParameterProvider : PreviewParameterProvider<Mechanic> {
+    override val values = sequenceOf(
+        Mechanic(
+            id = 99,
+            name = "Deck Building",
+            itemCount = 30,
+        ),
+        Mechanic(
+            id = 99,
+            name = "Auction",
+            itemCount = 0,
+        ),
+        Mechanic(
+            id = 99,
+            name = "Dice Rolling",
+            itemCount = 1,
+        )
+    )
 }
