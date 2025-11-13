7e9ac5e3162b3202657b4e9a4ee6cd1239043c43
Ivo Neskovic <ivo.neskovic@gmail.com>
2025-11-13 16:56:07 +0100

    trying to build the project once. icepick is an issue now

--- app/src/main/java/com/boardgamegeek/BggApplication.java
+++ app/src/main/java/com/boardgamegeek/BggApplication.java
@@ -7,7 +7,6 @@ import android.os.StrictMode.ThreadPolicy.Builder;
 import android.os.StrictMode.VmPolicy;
 
 import com.boardgamegeek.auth.AccountUtils;
-import com.boardgamegeek.events.BggEventBusIndex;
 import com.boardgamegeek.extensions.PreferenceUtils;
 import com.boardgamegeek.pref.SyncPrefs;
 import com.boardgamegeek.util.CrashReportingTree;
@@ -16,7 +15,7 @@ import com.boardgamegeek.util.NotificationUtils;
 import com.boardgamegeek.util.PreferencesUtils;
 import com.boardgamegeek.util.RemoteConfig;
 import com.facebook.stetho.Stetho;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
@@ -54,7 +53,7 @@ public class BggApplication extends MultiDexApplication {
 		EventBus.builder()
 			.logNoSubscriberMessages(BuildConfig.DEBUG)
 			.throwSubscriberException(BuildConfig.DEBUG)
-			.addIndex(new BggEventBusIndex())
+//			.addIndex(new BggEventBusIndex())
 			.installDefaultEventBus();
 
 		Picasso.setSingletonInstance(new Picasso.Builder(this)
@@ -67,8 +66,9 @@ public class BggApplication extends MultiDexApplication {
 		migrateCollectionStatusSettings();
 		SyncPrefs.migrate(this);
 
-		FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(instanceIdResult -> {
-			String deviceToken = instanceIdResult.getToken();
+		FirebaseMessaging.getInstance().getToken()
+			.addOnCompleteListener(task -> {
+			String deviceToken = task.getResult();
 			Timber.i("Firebase token is %s", deviceToken);
 		});
 	}
