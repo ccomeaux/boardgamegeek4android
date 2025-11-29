0e0030aff677a4deef3ebc922b5a0c4234af85cb
Chris Comeaux <chris.comeaux@gmail.com>
2025-11-28 19:26:34 -0500

    Update collection title and icon
    This commit updates the title for the legacy collection screen and replaces a Material Icon.
    
    The string `title_collection_legacy` in `strings.xml` has been changed from "Collection (Legacy)" to "Collection".
    
    In `SimpleCollectionItemList.kt`, the empty state view now uses a drawable resource (`R.drawable.collection_24px`) instead of the default Material Icon (`Icons.Default.Collections`).

--- app/src/main/java/com/boardgamegeek/ui/compose/SimpleCollectionItemList.kt
+++ app/src/main/java/com/boardgamegeek/ui/compose/SimpleCollectionItemList.kt
@@ -5,11 +5,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Collections
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.boardgamegeek.R
@@ -35,7 +34,7 @@ fun SimpleCollectionItemList(
         collectionItems.isEmpty() -> {
             EmptyFullSizeScrollableContent(
                 emptyTextResource,
-                Icons.Default.Collections,
+                painterResource(R.drawable.collection_24px),
                 padding = contentPadding,
             )
         }
